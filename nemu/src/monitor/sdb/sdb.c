ad147e7f73ead2b48a7ff697724b7897071200bc
tracer-ysyx <tracer@ysyx.org>
2025-03-27 22:12:34 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-21-generic #21-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 19 16:50:40 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:12:34 up  8:35,  2 users,  load average: 0.48, 0.47, 0.47

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -19,6 +19,7 @@
 #include <readline/history.h>
 #include <utils.h>
 #include <stdlib.h>
+#include <memory/paddr.h>
 #include "sdb.h"
 
 static int is_batch_mode = false;
@@ -68,6 +69,19 @@ static int cmd_info(char *args) {
   return 0;
 }
 
+static int cmd_x(char* args) {
+  int n = atoi(strtok(args, " "));
+  char* expr = strtok(NULL, " ");
+  paddr_t addr = 0;
+
+  addr = strtoul(expr, NULL, 16);
+  for(int i = 0; i < n; i++) {
+    printf("0x%-8x0x%-8x\n", addr, paddr_read(addr, 4));
+    addr += 4;
+  }
+  return 0;
+}
+
 static int cmd_q(char *args) {
   nemu_state.state = NEMU_END;
   return -1;
@@ -84,7 +98,8 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   {"si", "Let's the programepause after executing N instructions in a single step. N defaults to 1", cmd_si},
-  {"info", "Type r to print the register, type w for status watchpoint information", cmd_info}
+  {"info", "Type r to print the register, type w for status watchpoint information", cmd_info},
+  {"x", "x N EXPR, Scanning Memory, Outputs N consecutive 4 bytes starting from EXPR", cmd_x},
 
   /* TODO: Add more commands */
 
