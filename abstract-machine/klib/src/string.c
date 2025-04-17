534f89aaa0f37c8a9777fe67c94097ebce500660
tracer-ysyx <tracer@ysyx.org>
2025-04-17 16:55:46 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-21-generic #21-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 19 16:50:40 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:55:46 up 19:09,  2 users,  load average: 0.68, 0.43, 0.29

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -50,7 +50,6 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
   while (*s1 != '\0' && *s2 != '\0')
   {
     if(*s1 != *s2)
