423fa4ab46f1dbe33114a9c91a8228739b1cd09f
tracer-ysyx <tracer@ysyx.org>
2025-04-17 16:26:53 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-21-generic #21-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 19 16:50:40 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:26:53 up 18:40,  2 users,  load average: 0.60, 0.43, 0.47

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -102,9 +102,7 @@ static void statistic() {
 void iringbuf_print();
 
 void assert_fail_msg() {
-  printf("ASSERT_FAIL_MSG\n");
   iringbuf_print();
-  printf("ASSERT_FAIL_MSG\n");
   isa_reg_display();
   statistic();
 }
