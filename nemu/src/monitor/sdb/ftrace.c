b837fd8eb55854403d68ad2a8e00c84225092894
tracer-ysyx <tracer@ysyx.org>
2025-04-19 15:17:06 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-21-generic #21-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 19 16:50:40 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:17:06 up 2 days, 17:31,  2 users,  load average: 1.63, 1.13, 0.82

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -160,7 +160,7 @@ void ftrace_ret(word_t pc, word_t dnpc)
     --call_deep;
 
     int i = find_func(dnpc);
-    printf(FMT_PADDR ": %*sret [%s]\n", pc, call_deep * 2, "", i>=0?funcs[i].name:"???");
+    printf(FMT_PADDR ": %*sret [%s@" FMT_PADDR "]\n", pc, call_deep * 2, "", i>=0?funcs[i].name:"???", dnpc);
 }
 
 void ftrace_end()
