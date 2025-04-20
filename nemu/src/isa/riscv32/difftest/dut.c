364ee0233d1571114a25bef0738e880ad0119bc9
tracer-ysyx <tracer@ysyx.org>
2025-04-20 17:46:07 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-24-generic #24-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 14 18:13:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:46:07 up  4:41,  2 users,  load average: 0.62, 0.66, 0.51

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,9 +21,17 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = ARRLEN(cpu.gpr);
   for(int i = 0; i < reg_num; i++)
   {
-    if(ref_r->gpr[i] != cpu.gpr[i]) return false;
+    if(ref_r->gpr[i] != pc)
+    {
+      printf("Difftest: Inconsistent register values, pc = 0x%x\n", pc);
+      return false;
+    }
   }
-  if(ref_r->pc != cpu.pc) return false;
+  if(ref_r->pc != cpu.pc)
+  {
+    printf("Difftest: Inconsistent register values, pc = 0x%x\n", pc);
+    return false;
+  }
 
   return true;
 }
