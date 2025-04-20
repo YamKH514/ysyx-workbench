d4cbc0bd9372e98e86a83030a106f4e3a4046390
tracer-ysyx <tracer@ysyx.org>
2025-04-20 13:46:33 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.11.0-24-generic #24-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 14 18:13:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:46:33 up 42 min,  2 users,  load average: 0.77, 0.83, 0.60

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -32,6 +32,6 @@ int main(int argc, char *argv[]) {
   /* Start engine. */
   engine_start();
 
-  void ftrace_end();
+  IFDEF(CONFIG_FTRACE, {void ftrace_end();});
   return is_exit_status_bad();
 }
