0f9683c75ddbd271a5801546ea7501aafbe7363a
tracer-ysyx <tracer@ysyx.org>
2025-06-07 21:59:28 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-15-generic #15-Ubuntu SMP PREEMPT_DYNAMIC Sun Apr  6 15:05:05 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:59:28 up  7:56,  2 users,  load average: 0.87, 0.53, 0.41

--- npc/csrc/difftest/dut.c
+++ npc/csrc/difftest/dut.c
@@ -1,4 +1,5 @@
 #include <dlfcn.h>
+#include <sys/stat.h>
 
 #include "utils.h"
 #include "difftest-def.h"
@@ -110,5 +111,7 @@ void difftest_step(uint32_t pc, uint32_t npc) {
   checkregs(&ref_r, pc);
 }
 #else
-void init_difftest(char *ref_so_file, long img_size, int port) {printf("%ld\n", ftell((FILE *)ref_so_file)); }
+void init_difftest(char *ref_so_file, long img_size, int port) {
+    printf("diff: %s\n", ref_so_file);
+}
 #endif
