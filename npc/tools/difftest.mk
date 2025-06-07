3bd98a05983833b5ace7496c3498a9719a8b967c
tracer-ysyx <tracer@ysyx.org>
2025-06-07 21:47:42 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-15-generic #15-Ubuntu SMP PREEMPT_DYNAMIC Sun Apr  6 15:05:05 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:47:42 up  7:45,  2 users,  load average: 0.43, 0.31, 0.28

--- npc/tools/difftest.mk
+++ npc/tools/difftest.mk
@@ -1,8 +1,5 @@
 ifdef CONFIG_DIFFTEST
-DIFF_REF_PATH = $(NEMU_HOME)/build/test
-ifeq ($(wildcard $(DIFF_REF_PATH)),)
-	$(error $(DIFF_REF_PATH) 路径不存在)
-endif
+DIFF_REF_PATH = $(NEMU_HOME)/build
 DIFF_REF_SO = $(DIFF_REF_PATH)/riscv-nemu-interpreter-so
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 endif
