1a08dfc49b87bfd15f925657e6bc5e0993533dd5
tracer-ysyx <tracer@ysyx.org>
2025-06-09 21:09:25 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-15-generic #15-Ubuntu SMP PREEMPT_DYNAMIC Sun Apr  6 15:05:05 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:09:25 up 2 days,  7:06,  2 users,  load average: 1.05, 0.80, 0.78

--- npc/tools/difftest.mk
+++ npc/tools/difftest.mk
@@ -1,5 +1,5 @@
 ifdef CONFIG_DIFFTEST
 DIFF_REF_PATH = $(NEMU_HOME)/build
-DIFF_REF_SO = $(DIFF_REF_PATH)/riscv-nemu-interpreter-so
+DIFF_REF_SO = $(DIFF_REF_PATH)/riscv32-nemu-interpreter-so
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 endif
