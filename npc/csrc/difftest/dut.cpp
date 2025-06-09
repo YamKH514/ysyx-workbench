3ec732de0c8b0c9279763a0d249f62218f43d67e
tracer-ysyx <tracer@ysyx.org>
2025-06-09 21:14:09 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-15-generic #15-Ubuntu SMP PREEMPT_DYNAMIC Sun Apr  6 15:05:05 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:14:09 up 2 days,  7:11,  2 users,  load average: 1.23, 0.96, 0.85

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -22,9 +22,10 @@ void init_difftest(char *ref_so_file, long img_size, int port)
 
   void *handle;
   handle = dlopen(ref_so_file, RTLD_LAZY);
-  if (!handle) {
-  printf("dlopen failed: %s\n", dlerror());
-}
+  if (!handle)
+  {
+    printf("dlopen failed: %s\n", dlerror());
+  }
   assert(handle);
 
   ref_difftest_memcpy = (void (*)(uint32_t, void *, size_t, bool))dlsym(handle, "difftest_memcpy");
