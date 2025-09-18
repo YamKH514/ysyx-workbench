7298cb9c202ecdcd871f65c498421cf62765f5dc
tracer-ysyx <tracer@ysyx.org>
2025-09-18 14:29:26 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:29:26 up 4 days,  1:25,  2 users,  load average: 0.60, 0.54, 0.58

--- npc/vsrc/Decode.v
+++ npc/vsrc/Decode.v
@@ -29,6 +29,7 @@ wire inst_sltiu;    // I
 wire inst_slli;     // I
 wire inst_add;      // R
 wire inst_sub;      // R
+wire inst_sltu;     // R
 wire inst_xor;      // R
 wire inst_or;       // R
 wire inst_ebreak;
@@ -46,6 +47,7 @@ assign inst_sltiu = (Opcode == 7'b0010011) & (Funct3 == 3'b011);
 assign inst_slli  = (Opcode == 7'b0010011) & (Funct3 == 3'b001);
 assign inst_add   = (Opcode == 7'b0110011) & (Funct3 == 3'b000) & (Funct7 == 7'b0000000);
 assign inst_sub   = (Opcode == 7'b0110011) & (Funct3 == 3'b000) & (Funct7 == 7'b0100000);
+assign inst_sltu  = (Opcode == 7'b0110011) & (Funct3 == 3'b011) & (Funct7 == 7'b0000000);
 assign inst_xor   = (Opcode == 7'b0110011) & (Funct3 == 3'b100) & (Funct7 == 7'b0000000);
 assign inst_or    = (Opcode == 7'b0110011) & (Funct3 == 3'b110) & (Funct7 == 7'b0000000);
 assign inst_ebreak = inst == 32'b00000000000100000000000001110011;
@@ -63,13 +65,13 @@ assign InstType =   {3{inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli}
                     {3{inst_beq | inst_bne}} & 3'd2 | // B
                     {3{inst_lui | inst_auipc}} & 3'd3 | // U
                     {3{inst_jal}} & 3'd4 | // J
-                    {3{inst_add | inst_sub | inst_xor | inst_or}} & 3'd5; // R
+                    {3{inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 3'd5; // R
 
-assign RegWriteEn = inst_lui | inst_auipc | inst_jal | inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_xor | inst_or;
+assign RegWriteEn = inst_lui | inst_auipc | inst_jal | inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_sltu | inst_xor | inst_or;
 // AND(101000) OR(101110) XOR(100110)
 assign ALUFunc =    {6{inst_sub}} & 6'b000001 | // sub
                     {6{inst_beq | inst_bne}} & 6'b010011 | // A==B
-                    {6{inst_sltiu}} & 6'b010101 | // A<B
+                    {6{inst_sltiu | inst_sltu}} & 6'b010101 | // A<B
                     {6{1'b0}} & 6'b010111 | // A<=B
                     {6{1'b0}} & 6'b101000 | // AND
                     {6{inst_or}} & 6'b101110 | // OR
@@ -81,9 +83,9 @@ assign ALUFunc =    {6{inst_sub}} & 6'b000001 | // sub
 
 assign ALUSrcSel1 = {2{inst_lui}} & 2'd0 | // 0
                     {2{inst_jal | inst_jalr | inst_auipc}} & 2'd1 | // PC
-                    {2{inst_beq | inst_bne | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_xor | inst_or}} & 2'd2; // ReadData1
+                    {2{inst_beq | inst_bne | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 2'd2; // ReadData1
 
-assign ALUSrcSel2 = {2{inst_beq | inst_bne | inst_add | inst_sub | inst_xor | inst_or}} & 2'd0 | // ReadData2
+assign ALUSrcSel2 = {2{inst_beq | inst_bne | inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 2'd0 | // ReadData2
                     {2{inst_lui | inst_auipc | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli}} & 2'd1 | // ImmExt
                     {2{inst_jal | inst_jalr}} & 2'd2; // 4
 
