e3a081b86a34dc139f405a20cd3403f28dd003fe
tracer-ysyx <tracer@ysyx.org>
2025-09-18 21:29:55 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:29:55 up 4 days,  8:26,  2 users,  load average: 0.41, 0.46, 0.38

--- npc/vsrc/Decode.v
+++ npc/vsrc/Decode.v
@@ -27,6 +27,7 @@ wire inst_sw;       // S
 wire inst_addi;     // I
 wire inst_sltiu;    // I
 wire inst_slli;     // I
+wire inst_srai;     // I
 wire inst_add;      // R
 wire inst_sub;      // R
 wire inst_sltu;     // R
@@ -44,7 +45,8 @@ assign inst_lw    = (Opcode == 7'b0000011) & (Funct3 == 3'b010);
 assign inst_sw    = (Opcode == 7'b0100011) & (Funct3 == 3'b010);
 assign inst_addi  = (Opcode == 7'b0010011) & (Funct3 == 3'b000);
 assign inst_sltiu = (Opcode == 7'b0010011) & (Funct3 == 3'b011);
-assign inst_slli  = (Opcode == 7'b0010011) & (Funct3 == 3'b001);
+assign inst_slli  = (Opcode == 7'b0010011) & (Funct3 == 3'b001) & (Funct7 == 7'b0000000);
+assign inst_srai  = (Opcode == 7'b0010011) & (Funct3 == 3'b101) & (Funct7 == 7'b0100000);
 assign inst_add   = (Opcode == 7'b0110011) & (Funct3 == 3'b000) & (Funct7 == 7'b0000000);
 assign inst_sub   = (Opcode == 7'b0110011) & (Funct3 == 3'b000) & (Funct7 == 7'b0100000);
 assign inst_sltu  = (Opcode == 7'b0110011) & (Funct3 == 3'b011) & (Funct7 == 7'b0000000);
@@ -60,15 +62,15 @@ always @(posedge clk) begin
     end
 end
 
-assign InstType =   {3{inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli}} & 3'd0 | // I
+assign InstType =   {3{inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli | inst_srai}} & 3'd0 | // I
                     {3{inst_sw}} & 3'd1 | // S
                     {3{inst_beq | inst_bne}} & 3'd2 | // B
                     {3{inst_lui | inst_auipc}} & 3'd3 | // U
                     {3{inst_jal}} & 3'd4 | // J
                     {3{inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 3'd5; // R
 
-assign RegWriteEn = inst_lui | inst_auipc | inst_jal | inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_sltu | inst_xor | inst_or;
-// AND(101000) OR(101110) XOR(100110)
+assign RegWriteEn = inst_lui | inst_auipc | inst_jal | inst_jalr | inst_lw | inst_addi | inst_sltiu | inst_slli | inst_srai | inst_add | inst_sub | inst_sltu | inst_xor | inst_or;
+
 assign ALUFunc =    {6{inst_sub}} & 6'b000001 | // sub
                     {6{inst_beq | inst_bne}} & 6'b010011 | // A==B
                     {6{1'b0}} & 6'b010101 | // A<B signed
@@ -80,15 +82,15 @@ assign ALUFunc =    {6{inst_sub}} & 6'b000001 | // sub
                     {6{inst_xor}} & 6'b100110 | // XOR
                     {6{inst_slli}} & 6'b110000 | // SLL
                     {6{1'b0}} & 6'b110001 | // SRL
-                    {6{1'b0}} & 6'b110011 | // SRA
+                    {6{inst_srai}} & 6'b110011 | // SRA
                     6'b000000; // add
 
 assign ALUSrcSel1 = {2{inst_lui}} & 2'd0 | // 0
                     {2{inst_jal | inst_jalr | inst_auipc}} & 2'd1 | // PC
-                    {2{inst_beq | inst_bne | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli | inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 2'd2; // ReadData1
+                    {2{inst_beq | inst_bne | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli | inst_srai | inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 2'd2; // ReadData1
 
 assign ALUSrcSel2 = {2{inst_beq | inst_bne | inst_add | inst_sub | inst_sltu | inst_xor | inst_or}} & 2'd0 | // ReadData2
-                    {2{inst_lui | inst_auipc | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli}} & 2'd1 | // ImmExt
+                    {2{inst_lui | inst_auipc | inst_lw | inst_sw | inst_addi | inst_sltiu | inst_slli | inst_srai}} & 2'd1 | // ImmExt
                     {2{inst_jal | inst_jalr}} & 2'd2; // 4
 
 assign NPCSrcSel =  
