fba6efb5ed475dfec9c6ce3b0cb85075d92de904
tracer-ysyx <tracer@ysyx.org>
2025-09-20 21:14:50 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:14:50 up 6 days,  8:11,  2 users,  load average: 0.86, 0.78, 0.78

--- npc/vsrc/Shifter.v
+++ npc/vsrc/Shifter.v
@@ -32,13 +32,13 @@ module BarrelShifterLeft(
     output  [31:0]  ShifterRes
 );
 
-reg [31:0] s0, s1, s2, s3;
+wire [31:0] s0, s1, s2, s3;
 
-assign s0         = Shamt[0] ? {Src[31 -  1 : 0], 1'b0}  : Src;
-assign s1         = Shamt[1] ? {s0 [31 -  2 : 0], 2'b0}  : s0;
-assign s2         = Shamt[2] ? {s1 [31 -  4 : 0], 4'b0}  : s1;
-assign s3         = Shamt[3] ? {s2 [31 -  8 : 0], 8'b0}  : s2;
-assign ShifterRes = Shamt[4] ? {s3 [31 - 16 : 0], 16'b0} : s3;
+assign s0         = Shamt[0] ? {Src[30 : 0], 1'b0}  : Src;
+assign s1         = Shamt[1] ? {s0 [29 : 0], 2'b0}  : s0;
+assign s2         = Shamt[2] ? {s1 [27 : 0], 4'b0}  : s1;
+assign s3         = Shamt[3] ? {s2 [23 : 0], 8'b0}  : s2;
+assign ShifterRes = Shamt[4] ? {s3 [15 : 0], 16'b0} : s3;
 
 endmodule
 
@@ -50,7 +50,7 @@ module BarrelShifterRight(
     output  [31:0]  ShifterRes
 );
 
-reg [31:0] s0, s1, s2, s3;
+wire [31:0] s0, s1, s2, s3;
 wire msb = LASel ? Src[31] : 1'b0;
 
 assign s0         = Shamt[0] ? {{ 1{msb}}, Src[31 : 1]}  : Src;
