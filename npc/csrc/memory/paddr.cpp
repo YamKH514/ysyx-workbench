71211aa476cc08353ffec6bef4355869f5f5abea
tracer-ysyx <tracer@ysyx.org>
2025-09-23 11:31:36 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:31:36 up 20:58,  2 users,  load average: 0.97, 0.64, 0.52

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -8,6 +8,8 @@ static uint8_t pmem[MEM_MSIZE] PG_ALIGN = {};
 uint8_t *guest_to_host(uint32_t paddr) { return pmem + paddr - MEM_BASE; }
 uint32_t host_to_guest(uint8_t *haddr) { return haddr - pmem + MEM_BASE; }
 
+enum store{sb = 1, sh = 2, sw = 4};
+
 static uint32_t pmem_read(uint32_t addr, int len)
 {
     uint32_t ret = host_read(guest_to_host(addr), len);
@@ -57,29 +59,35 @@ extern "C" uint32_t paddr_read(uint32_t raddr)
 extern "C" void paddr_write(uint32_t waddr, uint32_t wdata, char wmask)
 {
     uint32_t addr = waddr & ~0x3u;
+    uint32_t data = 0;
+    uint32_t offset = waddr & 0x3;
     int len = 0;
     switch (wmask)
     {
     case 0x1:
-        len = 1;
+        len = sb;
+        data = (wdata & 0xF) << (offset * 4);
         break;
     case 0x3:
-        len = 2;
+        len = sh;
+        data = (wdata & 0xFF)  << (offset * 4);
         break;
     case 0xF:
-        len = 4;
+        len = sw;
+        data = wdata;
         break;
     default:
-        len = 4;
+        len = sw;
         break;
     }
 
+
 #ifdef CONFIG_MTRACE
-    print_paddr_write(addr, 4, wdata);
+    print_paddr_write(waddr, len, wdata);
 #endif
     if(likely(in_pmem(addr)))
     {
-        pmem_write(addr, 4, wdata);
+        pmem_write(addr, sw, wdata);
         return;
     }
     printf("paddr_write out_of_bound addr = 0x%x\n", addr);
