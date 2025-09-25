ef30f7596c7308e0414a0c0f9196636023ae3c66
tracer-ysyx <tracer@ysyx.org>
2025-09-25 11:45:47 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:45:47 up 2 days, 21:12,  2 users,  load average: 0.11, 0.22, 0.31

--- npc/vsrc/adder32.v
+++ npc/vsrc/adder32.v
@@ -8,10 +8,10 @@ module adder32(
     reg [31:0] b_complement, carry_out;
     wire [31:0] t_add_Cin;
 
-    assign b_complement = b ^ {32{mode}};
+    assign b_complement = b ^ {32{mode}} + {31'b0, mode};
 
     adder1 u_adder0(
-               .Cin  	(mode   ),
+               .Cin  	(0   ),
                .a    	(a[0]     ),
                .b    	(b_complement[0]     ),
                .Cout 	(carry_out[0]  ),
