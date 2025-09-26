af5215e788a54dd3df747806c9419d38927a8822
tracer-ysyx <tracer@ysyx.org>
2025-09-26 17:18:20 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:18:20 up 1 day,  2:01,  2 users,  load average: 0.53, 0.66, 0.48

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -57,7 +57,7 @@ char *int_to_str(int num, char *dest, int *cnt) {
 }
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
   char buf[128];
   char *out = buf;
   va_list args;
