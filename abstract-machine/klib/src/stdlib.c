ed496a967ac92e63978bce6f36821fde744cfd1d
tracer-ysyx <tracer@ysyx.org>
2025-09-28 22:33:00 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:33:00 up 1 day,  5:06,  2 users,  load average: 1.34, 1.05, 0.84

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -42,7 +42,6 @@ void *malloc(size_t size) {
     addr = (void *)ROUNDUP(heap.start, 8);
     addr_inited = 1;
   }
-  printf("klib malloc size = %lu\n", size);
   size = (size_t) ROUNDUP(size, 8);
   if((void *)addr + size >= heap.end)
   {
