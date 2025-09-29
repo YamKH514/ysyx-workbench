ae138b3cc830863f59eb5eb6a387fed642cdda26
tracer-ysyx <tracer@ysyx.org>
2025-09-29 16:20:29 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:20:29 up 1 day, 22:53,  2 users,  load average: 0.24, 0.57, 0.78

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -53,9 +53,16 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
   nr_map ++;
 }
 
+void print_devices_read(IOMap *map, paddr_t addr)
+{
+  printf("MMIO_READ at 0x%08x, device name is %s\n", addr, map->name);
+}
+
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  return map_read(addr, len, fetch_mmio_map(addr));
+  IOMap *map = fetch_mmio_map(addr);
+  IFDEF(CONFIG_DTRACE, print_devices_read(map, addr));
+  return map_read(addr, len, map);
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
