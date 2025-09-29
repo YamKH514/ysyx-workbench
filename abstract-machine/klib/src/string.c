a0ad6108621bbab7c3698982568eaf908b490251
tracer-ysyx <tracer@ysyx.org>
2025-09-29 14:01:32 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:01:32 up 1 day, 20:34,  2 users,  load average: 0.83, 0.94, 0.66

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -113,7 +113,13 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  const char *cin = in;
+  char *cout = out;
+  for(size_t i = 0; i < n; i++)
+  {
+    *(cout + i) = *(cin + i);
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
