0ef37ed9408c46c23e1f290a38cb9415e9c7b876
tracer-ysyx <tracer@ysyx.org>
2025-09-29 21:16:09 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:16:09 up 2 days,  3:49,  2 users,  load average: 0.34, 0.49, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,27 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 0;  // TODO: get the correct width
+  int h = 0;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t vga_clt = inl(VGACTL_ADDR);
+  uint32_t width = (vga_clt & 0xFFFF0000) >> 16;
+  uint32_t height = vga_clt & 0x0000FFFF;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
