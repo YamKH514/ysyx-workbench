ea77c736647eff3cb66bee3c3da1d8b1fd8d0b99
tracer-ysyx <tracer@ysyx.org>
2025-09-30 13:27:25 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 29 14:21:26 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:27:25 up 2 days, 20:00,  2 users,  load average: 0.73, 0.79, 0.64

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -67,6 +67,11 @@ extern "C" void paddr_write(uint32_t waddr, uint32_t wdata, uint8_t wmask)
     uint32_t addr = waddr & ~0x3u;
     uint32_t data = 0;
     uint32_t offset = waddr & 0x3;
+    if(addr == SERIAL_PORT)
+    {
+        putchar(wdata);
+        return;
+    }
     switch (wmask)
     {
     case 0x1:
@@ -87,11 +92,6 @@ extern "C" void paddr_write(uint32_t waddr, uint32_t wdata, uint8_t wmask)
 #ifdef CONFIG_MTRACE
     print_paddr_write(addr, 4, data);
 #endif
-    if(addr == SERIAL_PORT)
-    {
-        putchar(wdata);
-        return;
-    }
     if(likely(in_pmem(addr)))
     {
         pmem_write(addr, 4, data);
