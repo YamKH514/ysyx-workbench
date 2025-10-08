e5405a8ae0b178aca18151872bf3d0aa82c1d63a
tracer-ysyx <tracer@ysyx.org>
2025-10-08 20:04:24 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:04:24 up 33 min,  2 users,  load average: 0.97, 0.73, 0.64

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -49,13 +49,13 @@ extern "C" uint32_t paddr_read(uint32_t raddr)
 {
     uint32_t addr = raddr & ~0x3u;
     //TODO 无法按秒打印信息
-    if(addr == RTC_ADDR)
+    if(raddr == RTC_ADDR)
     {
         return (uint32_t)(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
     }
-    if(addr == RTC_ADDR + 0x4)
+    if(raddr == RTC_ADDR + 0x4)
     {
-        return (uint32_t)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() >> 32);
+        return (uint32_t)(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() >> 32);
     }
 #ifdef CONFIG_MTRACE
     print_paddr_read(addr, 4);
