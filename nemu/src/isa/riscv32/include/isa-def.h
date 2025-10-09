7ba3181fda95915e0f804e2a7dd69cd6c14e2862
tracer-ysyx <tracer@ysyx.org>
2025-10-09 20:32:46 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:32:46 up 1 day,  1:01,  2 users,  load average: 0.72, 0.59, 0.61

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -18,9 +18,12 @@
 
 #include <common.h>
 
+enum {mepc = 0, mcause, mtvec, mstatus};
+
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+  word_t csr[4];
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
