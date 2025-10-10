aae068d6dc1a9abd2fee340cc2daf32026f3fd14
tracer-ysyx <tracer@ysyx.org>
2025-10-10 20:48:02 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:48:02 up 2 days,  1:16,  2 users,  load average: 1.34, 1.07, 0.64

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -39,6 +39,11 @@ static debug_module_config_t difftest_dm_config = {
 struct diff_context_t {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   word_t pc;
+  // CSR
+  vaddr_t mepc;
+  word_t mcause;
+  word_t mtvec;
+  word_t mstatus;
 };
 
 static sim_t* s = NULL;
@@ -60,6 +65,11 @@ void sim_t::diff_get_regs(void* diff_context) {
     ctx->gpr[i] = state->XPR[i];
   }
   ctx->pc = state->pc;
+  // CSR
+  ctx->mepc = state->mepc->read();
+  ctx->mcause = state->mcause->read();
+  ctx->mtvec = state->mtvec->read();
+  ctx->mstatus = state->mstatus->read();
 }
 
 void sim_t::diff_set_regs(void* diff_context) {
@@ -68,6 +78,11 @@ void sim_t::diff_set_regs(void* diff_context) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
   state->pc = ctx->pc;
+  // CSR
+  state->mepc->write(ctx->mepc);
+  state->mcause->write(ctx->mcause);
+  state->mtvec->write(ctx->mtvec);
+  state->mstatus->write(ctx->mstatus);
 }
 
 void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
