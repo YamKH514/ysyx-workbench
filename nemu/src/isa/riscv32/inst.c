60476bc90197f9c26e080cab92d274a110bf8e69
tracer-ysyx <tracer@ysyx.org>
2025-10-10 19:07:40 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:07:40 up 1 day, 23:36,  2 users,  load average: 0.48, 0.65, 0.62

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -47,44 +47,6 @@ word_t *csr(word_t imm)
 #define immJ() do { *imm = (SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1), 21)); } while(0)
 #define immB() do { *imm = (SEXT(((BITS(i, 31, 31) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1)), 13)); } while(0)
 #define CSR(imm) *csr(imm)
-#define CSRR() do { \
-                    switch(imm) \
-                      { \
-                        case 0x300: \
-                          old = cpu.csr[mstatus]; \
-                          break; \
-                        case 0x305: \
-                          old = cpu.csr[mtvec]; \
-                          break; \
-                        case 0x341: \
-                          old = cpu.csr[mepc]; \
-                          break; \
-                        case 0x342: \
-                          old = cpu.csr[mcause]; \
-                          break; \
-                        default: \
-                          break; \
-                      } \
-                  } while(0)
-#define CSRW() do { \
-                    switch(imm) \
-                      { \
-                        case 0x300: \
-                          cpu.csr[mstatus] = src1; \
-                          break; \
-                        case 0x305: \
-                          cpu.csr[mtvec] = src1; \
-                          break; \
-                        case 0x341: \
-                          cpu.csr[mepc] = src1; \
-                          break; \
-                        case 0x342: \
-                          cpu.csr[mcause] = src1; \
-                          break; \
-                        default: \
-                          break; \
-                      } \
-                  } while(0)
 #define ECALL(dnpc) {bool success; dnpc = isa_raise_intr(isa_reg_str2val("a7", &success), s->pc); }
 
 void ftrace_call(word_t pc, word_t dnpc);
