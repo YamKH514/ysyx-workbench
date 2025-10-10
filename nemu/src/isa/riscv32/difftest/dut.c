2599c4f1fa288d12e879f240541bc0f963b998af
tracer-ysyx <tracer@ysyx.org>
2025-10-10 20:57:40 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:57:40 up 2 days,  1:26,  2 users,  load average: 0.90, 0.82, 0.68

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -47,11 +47,13 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->csr.mtvec != cpu.csr.mtvec)
   {
     printf("Difftest: Inconsistent MTVEC register values, pc = 0x%08x\n", pc);
+    printf("ref: 0x%08x, dut: 0x%08x\n", ref_r->csr.mtvec, cpu.csr.mtvec);
     return false;
   }
   if(ref_r->csr.mstatus != cpu.csr.mstatus)
   {
     printf("Difftest: Inconsistent MTATUS register values, pc = 0x%08x\n", pc);
+    printf("ref: 0x%08x, dut: 0x%08x\n", ref_r->csr.mstatus, cpu.csr.mstatus);
     return false;
   }
 
