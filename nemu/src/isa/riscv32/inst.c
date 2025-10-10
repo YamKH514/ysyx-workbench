84b8527a216154c92fedd313e7595eecc283c959
tracer-ysyx <tracer@ysyx.org>
2025-10-10 20:04:46 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:04:46 up 2 days, 33 min,  2 users,  load average: 0.22, 0.59, 0.46

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -132,7 +132,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , r, R(rd) = src1 & src2);
   INSTPAT("0000000 ????? ????? 101 ????? 00100 11", srli   , I, R(rd) = src1 >> (imm & 0x1F));
   INSTPAT("0100000 ????? ????? 101 ????? 00100 11", srai   , I, R(rd) = ((sword_t)src1) >> (imm & 0x1F));
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, printf("ECALL "); ECALL(s->dnpc); printf("dnpc = 0x%08x\n", s->dnpc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, printf("ECALL ");  printf("dnpc = 0x%08x\n", s->dnpc));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm); CSR(imm)  = src1);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSR(imm); CSR(imm) |= src1);
