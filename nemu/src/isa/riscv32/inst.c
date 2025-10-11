462d22d3d1cefcc0a2f946b66447c7208a54072d
tracer-ysyx <tracer@ysyx.org>
2025-10-11 21:57:04 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:57:04 up 3 days,  2:25,  2 users,  load average: 0.90, 0.86, 0.68

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -59,6 +59,8 @@ word_t *csr(word_t imm)
                   CSR(0x300) &= ~0x1800; \
                 }
 
+#define etrace() {printf("%s mepc: 0x%08x, mcause: 0x%08x\n", ANSI_FMT("ETRACE", ANSI_FG_YELLOW), cpu.csr.mepc, cpu.csr.mcause);}
+
 void ftrace_call(word_t pc, word_t dnpc);
 void ftrace_ret(word_t pc);
 
@@ -142,7 +144,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , r, R(rd) = src1 & src2);
   INSTPAT("0000000 ????? ????? 101 ????? 00100 11", srli   , I, R(rd) = src1 >> (imm & 0x1F));
   INSTPAT("0100000 ????? ????? 101 ????? 00100 11", srai   , I, R(rd) = ((sword_t)src1) >> (imm & 0x1F));
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(s->dnpc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(s->dnpc); etrace());
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm); CSR(imm)  = src1);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSR(imm); CSR(imm) |= src1);
