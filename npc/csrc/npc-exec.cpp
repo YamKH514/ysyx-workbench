c6bc07e4c9c28c74b9a21da29dc1b1aa75b41107
tracer-ysyx <tracer@ysyx.org>
2025-10-18 14:10:42 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:10:42 up 3 days, 16:36,  2 users,  load average: 0.44, 0.56, 0.56

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -124,7 +124,6 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
 #ifdef CONFIG_DIFFTEST
         difftest_step(npc);
 #endif
-        // if(top->pc == 0x80001054){printf("PC=0x%08x\n", top->pc);}
     }
 }
 
