a6bb0dbbc442c1e2c462596a881057f2a064b52c
tracer-ysyx <tracer@ysyx.org>
2025-10-19 13:59:12 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:59:12 up 4 days, 16:25,  2 users,  load average: 0.90, 1.04, 0.88

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -125,11 +125,8 @@ void difftest_step(uint32_t pc)
     return;
   }
 
-  printf("0\n");
   ref_difftest_exec(1);
-  printf("1\n");
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  printf("2\n");
   
   checkregs(&ref_r, pc);
 }
