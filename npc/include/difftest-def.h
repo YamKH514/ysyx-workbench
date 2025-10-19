86d02334dca76ddde52c39ea20e737b084db84b1
tracer-ysyx <tracer@ysyx.org>
2025-10-19 12:10:13 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:13 up 4 days, 14:36,  2 users,  load average: 2.34, 0.98, 0.40

--- npc/include/difftest-def.h
+++ npc/include/difftest-def.h
@@ -13,6 +13,6 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 #define DIFFTEST_REG_SIZE (sizeof(RISCV_GPR_TYPE) * (RISCV_GPR_NUM + 1)) // GPRs + pc
 
 void init_difftest(char *ref_so_file, long img_size, int port);
-void difftest_step(uint32_t pc);
+void difftest_step(uint32_t pc, uint32_t npc);
 
 #endif
