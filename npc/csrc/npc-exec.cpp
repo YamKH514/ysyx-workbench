92cf7157f2296e3b0c4999367abdef3ce31f3541
tracer-ysyx <tracer@ysyx.org>
2025-10-19 15:36:17 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:17 up 4 days, 18:02,  2 users,  load average: 0.54, 0.66, 0.61

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -53,11 +53,11 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
     uint32_t npc = top->npc;
     npc_state.halt_ret = top->ReadData_a0;
     contextp->timeInc(1);
-    top->clk = 1;
+    top->clk = 0;
     top->eval();
     tfp->dump(contextp->time());
     contextp->timeInc(1);
-    top->clk = 0;
+    top->clk = 1;
     top->eval();
     tfp->dump(contextp->time());
 
@@ -130,7 +130,7 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
 #endif
         }
 #ifdef CONFIG_DIFFTEST
-        difftest_step(npc_state.halt_pc);
+        difftest_step(npc);
 #endif
     }
 }
