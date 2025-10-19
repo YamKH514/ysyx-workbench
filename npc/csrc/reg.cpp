d28a04fde135e3154f8ac96d66a379cf3248485e
tracer-ysyx <tracer@ysyx.org>
2025-10-19 16:08:27 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:08:27 up 4 days, 18:34,  2 users,  load average: 0.77, 0.83, 0.73

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -1,4 +1,6 @@
 #include "reg.h"
+#include "Vtop.h"
+#include "Vtop__Dpi.h"
 
 const char *regs[] = {
     "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
@@ -7,8 +9,11 @@ const char *regs[] = {
 int reg_length = sizeof(regs) / sizeof(regs[0]);
 void reg_display()
 {
+    int gpr[16] = {0};
+    svSetScope(svGetScopeFromName("TOP.top.u_GPR.u_RegisterFile"));
+    get_gpr(gpr);
     for (int i = 0; i < reg_length; i++)
     {
-        printf("%-10s 0x%08x  %-10u\n", regs[i], gpr(i), gpr(i));
+        printf("%-10s 0x%08x  %-10u\n", regs[i], gpr[i], gpr[i]);
     }
 }
