e64fad42d312f39847fcff06405a9860632ecb8f
tracer-ysyx <tracer@ysyx.org>
2025-10-19 15:56:32 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:56:32 up 4 days, 18:22,  2 users,  load average: 0.61, 0.57, 0.65

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -44,11 +44,11 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
     {
         top->rst = 1;
         contextp->timeInc(1);
-        top->clk = 1;
+        top->clk = 0;
         top->eval();
         tfp->dump(contextp->time());
         contextp->timeInc(1);
-        top->clk = 0;
+        top->clk = 1;
         top->eval();
         tfp->dump(contextp->time());
         top->rst = 0;
