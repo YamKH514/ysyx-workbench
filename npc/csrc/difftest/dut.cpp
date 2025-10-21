ef2ed6b410fd2669800195fd2494a3f787754836
tracer-ysyx <tracer@ysyx.org>
2025-10-21 18:56:16 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:56:16 up 6 days, 21:22,  2 users,  load average: 1.16, 0.91, 0.49

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -15,11 +15,9 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 #ifdef CONFIG_DIFFTEST
 
 static bool is_skip_ref = false;
-static int skip_dut_nr_inst = 0;
 
 void difftest_skip_ref() {
   is_skip_ref = true;
-  skip_dut_nr_inst = 0;
 }
 
 void init_difftest(char *ref_so_file, long img_size, int port)
