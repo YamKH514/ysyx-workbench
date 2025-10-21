eea201f72ad13e9ffd9198ccedb316fa7b12f061
tracer-ysyx <tracer@ysyx.org>
2025-10-21 12:59:19 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:59:19 up 6 days, 15:25,  2 users,  load average: 0.91, 0.71, 0.37

--- npc/include/npc-init.h
+++ npc/include/npc-init.h
@@ -3,6 +3,10 @@
 
 #include "common.h"
 
+#include "Vtop.h"
+#include "verilated_vcd_c.h"
+#include "Vtop__Dpi.h"
+
 void init_npc(int argc, char *argv[], Vtop* top, VerilatedContext* contextp, VerilatedVcdC* tfp);
 
 #endif
