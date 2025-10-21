dd42802d5dd2f9cb852e76f16c60c80d76e962f3
tracer-ysyx <tracer@ysyx.org>
2025-10-21 22:24:12 +0800

    > compile NEMU ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:24:12 up 7 days, 50 min,  2 users,  load average: 1.27, 0.73, 0.48

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -74,7 +74,6 @@ void sim_t::diff_get_regs(void* diff_context) {
 
 void sim_t::diff_set_regs(void* diff_context) {
   struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
-  printf("ctx.pc = 0x%08x\n", ctx->pc);
   for (int i = 0; i < NR_GPR; i++) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
