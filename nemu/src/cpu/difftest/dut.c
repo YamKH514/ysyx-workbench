03ceadc758a217c37199a080f123a39c1cd05c4c
tracer-ysyx <tracer@ysyx.org>
2025-10-21 13:41:03 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:41:03 up 6 days, 16:07,  2 users,  load average: 1.82, 1.01, 0.83

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -88,7 +88,6 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
   ref_difftest_init(port);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-  printf("&cpu.pc = 0x%08x\n", cpu.pc);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
@@ -118,7 +117,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 
   if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to reference design
-    printf("difftest skip pc = 0x%08x\n", pc);
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -50,8 +50,9 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
     top->clk = 1;
     top->eval();
     tfp->dump(contextp->time());
-    cpu.pc = top->pc;
 
+    cpu.pc = top->pc;
+    printf("cpu.pc = 0x%08x, halt_pc = 0x%08x\n", cpu.pc, npc_state.halt_pc);
     svSetScope(svGetScopeFromName("TOP.top.u_GPR.u_RegisterFile"));
     get_gpr(cpu.gpr);
     svSetScope(svGetScopeFromName("TOP.top.u_CSR"));
