286ab1783472708dcee69ece4dcf55a4e7505abd
tracer-ysyx <tracer@ysyx.org>
2025-10-22 19:35:00 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:35:00 up 7 days, 22:01,  2 users,  load average: 1.12, 0.93, 0.68

--- npc/vsrc/Memory.v
+++ npc/vsrc/Memory.v
@@ -19,7 +19,9 @@ wire    [15:0]  DataH;
 import "DPI-C" function int paddr_read(input int raddr);
 import "DPI-C" function void paddr_write(
     input int waddr, input int wdata, input byte wmask);
+`ifdef CONFIG_DIFFTEST
 import "DPI-C" function void difftest_skip_ref(int pc);
+`endif
 always @(posedge clk) begin
     Data <= 0;
     if (MemValid) begin // 有读写请求时
@@ -30,9 +32,11 @@ always @(posedge clk) begin
             Data <= paddr_read(raddr);
         end
     end
-    // if ((raddr == 32'ha0000048) | (raddr == 32'ha0000048 + 32'h4) | (waddr == 32'ha00003f8)) begin
-    //     difftest_skip_ref(npc); // Difftest跳过读写设备
-    // end
+`ifdef CONFIG_DIFFTEST
+    if ((raddr == 32'ha0000048) | (raddr == 32'ha0000048 + 32'h4) | (waddr == 32'ha00003f8)) begin
+        difftest_skip_ref(npc); // Difftest跳过读写设备
+    end
+`endif
 end
 
 assign ByteOff = raddr[1:0];
