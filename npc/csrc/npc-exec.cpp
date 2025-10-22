fed25521713d59d7f211e59b749615d6074a1ba9
tracer-ysyx <tracer@ysyx.org>
2025-10-22 16:47:14 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:47:14 up 7 days, 19:13,  2 users,  load average: 0.87, 1.15, 0.97

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -44,11 +44,11 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
     npc_state.halt_ret = top->ReadData_a0;
     contextp->timeInc(1);
     top->rst = 0;
-    top->clk = 0;
+    top->clk = 1;
     top->eval();
     tfp->dump(contextp->time());
     contextp->timeInc(1);
-    top->clk = 1;
+    top->clk = 0;
     top->eval();
     tfp->dump(contextp->time());
 
--- npc/csrc/npc-init.cpp
+++ npc/csrc/npc-init.cpp
@@ -5,8 +5,6 @@
 #include "disasm.h"
 #include "difftest-def.h"
 #include "sdb.h"
-#include "cpu.h"
-#include "Vtop__Dpi.h"
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
@@ -92,14 +90,12 @@ void reset_npc(Vtop* top, VerilatedContext* contextp, VerilatedVcdC* tfp)
         top->eval();
         tfp->dump(contextp->time());
         contextp->timeInc(1);
+        top->clk = 0;
+        top->eval();
+        tfp->dump(contextp->time());
         printf("inited pc = 0x%08x\n", top->pc);
         npc_state.halt_pc = top->pc;
         npc_state.inited = true;
-        cpu.pc = top->npc;
-        svSetScope(svGetScopeFromName("TOP.top.u_GPR.u_RegisterFile"));
-        get_gpr(cpu.gpr);
-        svSetScope(svGetScopeFromName("TOP.top.u_CSR"));
-        get_csr((int *)(&cpu.csr));
     }
 }
 
@@ -124,10 +120,6 @@ void init_npc(int argc, char *argv[], Vtop* top, VerilatedContext* contextp, Ver
     init_sdb(top, contextp, tfp);
 
     init_disasm();
-
-#ifdef CONFIG_DIFFTEST
-    difftest_step(npc_state.halt_pc);
-#endif
 
     Log("init_npc has done.");
 }
