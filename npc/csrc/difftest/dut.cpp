040e6292197b5b2fd6cfe7b0b26052c0be5fdac1
tracer-ysyx <tracer@ysyx.org>
2025-10-22 17:01:40 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:01:40 up 7 days, 19:27,  2 users,  load average: 1.24, 1.36, 1.18

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -76,6 +76,7 @@ bool difftest_checkregs(CPU_state*ref_r, uint32_t pc)
   if (ref_r->pc != cpu.pc)
   {
     printf("Difftest: Inconsistent PC register values, pc = 0x%08x\n", pc);
+    printf("ref: 0x%08x, dut: 0x%08x\n", ref_r->pc, cpu.pc);
     return false;
   }
   if(ref_r->csr.mepc != cpu.csr.mepc)
