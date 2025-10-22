053c71b1ef7e3b5790a40559947a83a6b33fbf38
tracer-ysyx <tracer@ysyx.org>
2025-10-22 20:42:36 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:42:36 up 7 days, 23:08,  2 users,  load average: 2.39, 1.58, 1.16

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -82,11 +82,8 @@ extern "C" void paddr_write(uint32_t waddr, uint32_t wdata, uint8_t wmask)
     uint32_t addr = waddr & ~0x3u;
     uint32_t data = 0;
     uint32_t offset = waddr & 0x3;
-    printf("w addr = 0x%08x\n", addr);
     if(addr == SERIAL_PORT)
     {
-        printf("serial used pc = 0x%08x\n", cpu.pc);
-        printf("DBG paddr_write: dut pc(before) = 0x%08x, skip_to_will = 0x%08x\n", cpu.pc, cpu.pc+4);
         putchar(wdata);
         fflush(stdout);
 #ifdef CONFIG_DIFFTEST
