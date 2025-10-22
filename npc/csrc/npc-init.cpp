b8f780a833125d7e17a81292af33c99ba82adb2a
tracer-ysyx <tracer@ysyx.org>
2025-10-22 16:35:21 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:35:21 up 7 days, 19:01,  2 users,  load average: 0.95, 1.20, 0.78

--- npc/csrc/npc-init.cpp
+++ npc/csrc/npc-init.cpp
@@ -91,6 +91,7 @@ void reset_npc(Vtop* top, VerilatedContext* contextp, VerilatedVcdC* tfp)
         tfp->dump(contextp->time());
         contextp->timeInc(1);
         top->rst = 0;
+        top->clk = 0;
         top->eval();
         tfp->dump(contextp->time());
         npc_state.inited = true;
