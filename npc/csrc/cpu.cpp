16dd39055184b83e69443cc1f157f4b3a18b1cd2
tracer-ysyx <tracer@ysyx.org>
2025-10-22 17:15:23 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-33-generic #33-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 23:22:02 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:15:23 up 7 days, 19:41,  2 users,  load average: 0.34, 0.49, 0.77

---
+++ npc/csrc/cpu.cpp
@@ -0,0 +1,8 @@
+// #include "common.h"
+// #include "cpu.h"
+// #include "Vtop__Dpi.h"
+
+// extern "C" void get_cpu_state()
+// {
+
+// }
--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -59,16 +59,19 @@ static void single_cycle(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *t
     tfp->dump(contextp->time());
     npc_state.halt_pc = top->pc;
     npc_state.halt_ret = top->ReadData_a0;
+
+    cpu.pc = top->npc;
+    svSetScope(svGetScopeFromName("TOP.top.u_GPR.u_RegisterFile"));
+    get_gpr(cpu.gpr);
+    svSetScope(svGetScopeFromName("TOP.top.u_CSR"));
+    get_csr((int *)(&cpu.csr));
+
     contextp->timeInc(1);
     top->clk = 1;
     top->eval();
     tfp->dump(contextp->time());
 
-    cpu.pc = top->pc;
-    svSetScope(svGetScopeFromName("TOP.top.u_GPR.u_RegisterFile"));
-    get_gpr(cpu.gpr);
-    svSetScope(svGetScopeFromName("TOP.top.u_CSR"));
-    get_csr((int *)(&cpu.csr));
+    
 
     if (npc_state.halt_pc >= 0x80000000)
     {
