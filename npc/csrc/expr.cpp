58a55237a2bf51a7624cab739acfe085f75493dc
tracer-ysyx <tracer@ysyx.org>
2025-10-27 13:50:50 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-34-generic #34-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 09:21:29 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:50:50 up 2 days, 21:21,  2 users,  load average: 1.20, 0.98, 0.82

--- npc/csrc/expr.cpp
+++ npc/csrc/expr.cpp
@@ -86,9 +86,9 @@ static bool make_token(char *e)
         /* Try all rules one by one. */
         for (i = 0; i < NR_REGEX; i++)
         {
-            printf("1\n");
             if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
             {
+                printf("1\n");
                 char *substr_start = e + position;
                 int substr_len = pmatch.rm_eo;
 
