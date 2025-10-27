e14da6e2f74c2cd0be34565e6691b78612f8d961
tracer-ysyx <tracer@ysyx.org>
2025-10-27 13:27:47 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-34-generic #34-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 09:21:29 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:27:47 up 2 days, 20:58,  2 users,  load average: 0.70, 0.95, 1.05

---
+++ npc/include/expr.h
@@ -0,0 +1,8 @@
+#ifndef EXPR_H__
+#define EXPR_H__
+
+#include "common.h"
+
+uint32_t expr(char *e, bool *success);
+
+#endif
---
+++ npc/include/watchpoint.h
@@ -0,0 +1,21 @@
+#ifndef __WATCHPOINT_H__
+#define __WATCHPOINT_H__
+
+#define NR_WP 32
+
+#include "common.h"
+
+typedef struct watchpoint
+{
+    int NO;
+    struct watchpoint *next;
+    char expression[1000];
+    uint32_t old_value;
+} WP;
+
+WP *new_wp();
+void free_wp(int n);
+bool wp_scan();
+void wp_info();
+
+#endif
