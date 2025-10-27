6685c7a3d71b8f524a5178cef5c779e3e3573d02
tracer-ysyx <tracer@ysyx.org>
2025-10-27 13:29:03 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-34-generic #34-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 17 09:21:29 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:29:03 up 2 days, 20:59,  2 users,  load average: 1.23, 1.04, 1.07

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -85,7 +85,7 @@ static int cmd_x(char *args)
 }
 
 static int cmd_p(char *args) {
-    word_t res;
+    uint32_t res;
     bool *success;
     success = (bool *)malloc(sizeof(bool));
     res = expr(args, success);
