de23c405171138d31587f34bd7297a10b1631a03
Ren Kunhou <r2726992334@gmail.com>
2025-10-29 21:47:47 +0800

    修复立即数解码错误

--- npc/vsrc/ImmDecode.v
+++ npc/vsrc/ImmDecode.v
@@ -6,10 +6,10 @@ module ImmDecode(
 
 assign ImmExt = 
             (InstType == 3'd0) ? {{20{Imm[31]}}, Imm[31:20]} :
-            (InstType == 3'd1) ? {{20{Imm[31]}}, Imm[31:25], Imm[11:8], Imm[7]} :
-            (InstType == 3'd2) ? {{18{Imm[31]}}, {3{Imm[7]}}, Imm[30:25], Imm[11:8], 1'b0} :
+            (InstType == 3'd1) ? {{20{Imm[31]}}, Imm[31:25], Imm[11:7]} :
+            (InstType == 3'd2) ? {{19{Imm[31]}}, Imm[31], Imm[7], Imm[30:25], Imm[11:8], 1'b0} :
             (InstType == 3'd3) ? {Imm[31:12], 12'b0} :
-            (InstType == 3'd4) ? {{12{Imm[31]}}, Imm[19:12], Imm[20], Imm[30:21], 1'b0} :
+            (InstType == 3'd4) ? {{11{Imm[31]}}, Imm[31], Imm[19:12], Imm[20], Imm[30:21], 1'b0} :
             32'b0;
 
 endmodule
