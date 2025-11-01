9aa072d43dd11607768c27746353f42504b7ed15
tracer-ysyx <tracer@ysyx.org>
2025-11-01 18:57:08 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.14.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Sat Oct 11 10:06:31 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:57:08 up  8:04,  2 users,  load average: 0.47, 0.90, 0.69

--- npc/vsrc/InstSRAM.v
+++ npc/vsrc/InstSRAM.v
@@ -6,13 +6,13 @@ module InstSRAM (
     output  reg         inst_sram_ready_out
 );
 
-wire [31:0]  data;
+reg [31:0]  data;
 
 import "DPI-C" function int get_inst(input int pc);
 assign data = get_inst(inst_sram_addr_in);
 
 always @(posedge inst_sram_clk_in) begin
-    if (inst_sram_valid_in & (data != 32'b0)) begin
+    if (inst_sram_valid_in) begin
         inst_sram_data_out  <= data;
         inst_sram_ready_out <= 1;
     end else begin
