7a45db89b7dc80919fd86dc8b6e787113a511314
tracer-ysyx <tracer@ysyx.org>
2025-11-24 20:58:54 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.17.0-6-generic #6-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct  7 13:34:17 UTC 2025 x86_64 GNU/Linux  20:58:54 up 6 days,  5:53,  1 user,  load average: 1.97, 1.28, 1.04

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -131,7 +131,7 @@ void difftest_step(uint32_t pc)
   if(is_skip_ref)
   {
     skip_pc_queue[skip_pc_tail] = (uint32_t)need_skip_pc;
-    printf("recode skip pc: 0x%08x\n", skip_pc);
+    printf("recode skip pc: 0x%08x\n", need_skip_pc);
     skip_pc_tail = (skip_pc_tail + 1) % MAX_SKIP_NUM;
     is_skip_ref = false;
   }
