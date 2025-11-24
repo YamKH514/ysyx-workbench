edc728856a010442594ba6beeba7f36f23c58c10
tracer-ysyx <tracer@ysyx.org>
2025-11-24 21:15:46 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.17.0-6-generic #6-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct  7 13:34:17 UTC 2025 x86_64 GNU/Linux  21:15:46 up 6 days,  6:10,  1 user,  load average: 1.71, 1.23, 1.38

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -68,6 +68,7 @@ static void exec_once(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *tfp)
     {
         npc_state.halt_pc = top->pc;
         npc_state.halt_ret = top->ReadData_a0;
+        printf("halt_pc: 0x%08x\n", npc_state.halt_pc);
     }
 
     contextp->timeInc(1);
@@ -84,6 +85,7 @@ static void exec_once(Vtop *top, VerilatedContext *contextp, VerilatedVcdC *tfp)
         get_gpr(cpu.gpr);
         svSetScope(svGetScopeFromName("TOP.top.u_CSR"));
         get_csr((int *)(&cpu.csr));
+        printf("cpu.pc: 0x%08x\n", cpu.pc);
     }
 
     if (npc_state.halt_pc >= 0x80000000)
