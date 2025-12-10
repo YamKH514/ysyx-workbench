e6c1b8cd428b49ed1d66a58b59ce61f52e1fac73
tracer-ysyx <tracer@ysyx.org>
2025-12-10 17:18:36 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.17.0-7-generic #7-Ubuntu SMP PREEMPT_DYNAMIC Sat Oct 18 10:10:29 UTC 2025 x86_64 GNU/Linux  17:18:36 up 1 day,  2:53,  1 user,  load average: 0.61, 0.73, 0.82

--- npc/include/Vtop__Dpi.h
+++ npc/include/Vtop__Dpi.h
@@ -6,7 +6,7 @@
 extern void ebreak_trigger();
 extern uint32_t paddr_read(uint32_t raddr);
 extern void paddr_write(uint32_t waddr, uint32_t wdata, char wmask);
-extern void uart_difftest_skip(uint32_t waddr);
+extern void uart_difftest_skip();
 extern void get_gpr(int out_gpr[16]);
 extern void get_csr(int csr[4]);
 
--- npc/vsrc/UART.v
+++ npc/vsrc/UART.v
@@ -1,7 +1,7 @@
 module UART(
     /* verilator lint_off UNUSEDSIGNAL */
-    input clk,
-    input rstn,
+    input               clk,
+    input               rstn,
 
     // AR
     input       [31:0]  araddr_in,
@@ -31,33 +31,57 @@ module UART(
     input               bready_in
 );
 
-// import "DPI-C" function void uart_difftest_skip(input int waddr);
-import "DPI-C" function void paddr_write(
-    input int waddr, input int wdata, input byte wmask);
+import "DPI-C" function void uart_difftest_skip();
 
 reg [31:0]  awaddr_r;
-// reg [31:0]  wdata_r;
 
 parameter S_IDLE   = 3'd0;
+parameter S_GET_AR = 3'd1;
+parameter S_SEND_R = 3'd2;
 parameter S_GET_WR = 3'd3;
 parameter S_GET_WD = 3'd4;
 parameter S_SEND_B = 3'd5;
 
+reg [2:0]   r_state, r_next_state;
 reg [2:0]   w_state, w_next_state;
 
 always @(posedge clk) begin
     if (!rstn) begin
+        r_state <= S_IDLE;
         w_state <= S_IDLE;
     end else begin
+        r_state <= r_next_state;
         w_state <= w_next_state;
     end
 
     // READ
     if (!rstn) begin
-        arready_out <= 1'b0;
+        arready_out <= 1'b1;
+        rvalid_out  <= 1'b0;
         rdata_out   <= 32'b0;
         rresp_out   <= 2'b00;
-        rvalid_out  <= 1'b0;
+    end else begin
+        case (r_state)
+            S_IDLE: begin
+                if (arvalid_in & arready_out) begin
+                    arready_out <= 1'b0;
+                end
+            end
+            S_GET_AR: begin
+                rvalid_out <= 1'b1;
+                rresp_out  <= 2'b00;
+            end
+            S_SEND_R: begin
+                if (rvalid_out & rready_in) begin
+                    arready_out <= 1'b1;
+                    rvalid_out  <= 1'b0;
+                end
+            end
+            default: begin
+                arready_out <= 1'b1;
+                rvalid_out  <= 1'b0;
+            end
+        endcase
     end
 
     // WRITE
@@ -72,7 +96,6 @@ always @(posedge clk) begin
                 if (awvalid_in & awready_out) begin
                     awaddr_r    <= awaddr_in;
                     awready_out <= 1'b0;
-                    $display("UART");
                 end
             end
             S_GET_WR: begin
@@ -101,7 +124,28 @@ always @(posedge clk) begin
 end
 
 always @(*) begin
+    r_next_state = r_state;
     w_next_state = w_state;
+
+    // READ
+    case (r_state)
+        S_IDLE: begin
+            if (arvalid_in & arready_out) begin
+                r_next_state = S_GET_AR;
+            end
+        end
+        S_GET_AR: begin
+            r_next_state = S_SEND_R;
+        end
+        S_SEND_R: begin
+            if (rvalid_out & rready_in) begin
+                r_next_state = S_IDLE;
+            end
+        end
+        default: begin
+            r_next_state = S_IDLE;
+        end
+    endcase
 
     // WRITE
     case (w_state)
@@ -116,7 +160,9 @@ always @(*) begin
             end
         end
         S_GET_WD: begin
-            paddr_write(awaddr_r, wdata_in, {4'b0, wstrb_in});
+            $write("c", wdata_in);
+            uart_difftest_skip();
+            // paddr_write(awaddr_r, wdata_in, {4'b0, wstrb_in});
             w_next_state = S_SEND_B;
         end
         S_SEND_B: begin
