c02f322f56bbf8c3b2704304c451b8b658e60e92
tracer-ysyx <tracer@ysyx.org>
2025-12-10 12:14:30 +0800

    > sim RTL ysyx_22040000 任堃厚 Linux ren-desktop 6.17.0-7-generic #7-Ubuntu SMP PREEMPT_DYNAMIC Sat Oct 18 10:10:29 UTC 2025 x86_64 GNU/Linux  12:14:30 up 21:49,  1 user,  load average: 0.47, 0.21, 0.12

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -445,7 +445,7 @@ SRAM u_SRAM(
     .bready_in   	        (sram_bready        )
 );
 
-UART u_UART(
+SRAM u_UART(
     .clk         	        (clk                ),
     .rstn        	        (rstn               ),
     .araddr_in   	        (uart_araddr        ),
