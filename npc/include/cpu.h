22f90e6c7d1f1d061353f378f444bf32f01c65b3
tracer-ysyx <tracer@ysyx.org>
2025-12-24 19:46:42 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  19:46:42 up 5 days, 20:40,  1 user,  load average: 2.06, 1.83, 1.55

--- npc/include/cpu.h
+++ npc/include/cpu.h
@@ -23,7 +23,7 @@ typedef struct {
 
 extern CPU_state cpu;
 
-void cpu_single_cycle(Vtop* top);
+void cpu_single_cycle(Vtop* top, VerilatedContext *contextp, VerilatedVcdC *tfp);
 void cpu_reset(int n, Vtop* top, VerilatedContext *contextp, VerilatedVcdC *tfp);
 
 #endif
