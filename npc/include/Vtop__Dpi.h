9456b9cf83c29385621ff24b8ca88168ea1fe149
tracer-ysyx <tracer@ysyx.org>
2025-12-25 11:47:49 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  11:47:49 up 6 days, 12:41,  1 user,  load average: 3.25, 2.83, 2.41

--- npc/include/Vtop__Dpi.h
+++ npc/include/Vtop__Dpi.h
@@ -3,6 +3,10 @@
 
 #include "common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void ebreak_trigger();
 extern uint32_t paddr_read(uint32_t raddr);
 extern void paddr_write(uint32_t waddr, uint32_t wdata, char wmask);
@@ -10,5 +14,9 @@ extern void get_gpr(int out_gpr[16]);
 extern void get_csr(int csr[4]);
 extern void uart_difftest_skip();
 extern void clint_difftest_skip();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
