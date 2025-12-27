776d651d15d914e0f861f89bf6ddd2cf87e601e0
tracer-ysyx <tracer@ysyx.org>
2025-12-27 22:27:12 +0800

    > compile NEMU ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  22:27:12 up  4:30,  1 user,  load average: 1.11, 2.30, 2.46

--- nemu/include/memory/paddr.h
+++ nemu/include/memory/paddr.h
@@ -20,7 +20,11 @@
 
 #define PMEM_LEFT  ((paddr_t)CONFIG_MBASE)
 #define PMEM_RIGHT ((paddr_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
-#define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
+#define MROM_LEFT  ((paddr_t)CONFIG_MROMBASE)
+#define MROM_RIGHT ((paddr_t)CONFIG_MROMBASE + CONFIG_MROMSIZE - 1)
+#define SRAM_LEFT  ((paddr_t)CONFIG_SRAMBASE)
+#define SRAM_RIGHT ((paddr_t)CONFIG_SRAMBASE + CONFIG_SRAMSIZE - 1)
+#define RESET_VECTOR MUXDEF(CONFIG_YSYXSOC, (MROM_LEFT), (PMEM_LEFT + CONFIG_PC_RESET_OFFSET))
 
 /* convert the guest physical address in the guest program to host virtual address in NEMU */
 uint8_t* guest_to_host(paddr_t paddr);
@@ -28,7 +32,13 @@ uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
 
 static inline bool in_pmem(paddr_t addr) {
+#ifdef CONFIG_YSYXSOC
+  bool in_mrom = (MROM_LEFT <= addr) && (addr < MROM_RIGHT);
+  bool in_sram = (SRAM_LEFT <= addr) && (addr < SRAM_RIGHT);
+  return in_mrom | in_sram;
+#else
   return addr - CONFIG_MBASE < CONFIG_MSIZE;
+#endif
 }
 
 word_t paddr_read(paddr_t addr, int len);
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -19,13 +19,41 @@
 #include <isa.h>
 
 #if   defined(CONFIG_PMEM_MALLOC)
+#ifdef CONFIG_YSYXSOC
+static uint8_t *mrom = NULL;
+static uint8_t *sram = NULL;
+#endif
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
+#ifdef CONFIG_YSYXSOC
+static uint8_t mrom[CONFIG_MROMSIZE] PG_ALIGN = {};
+static uint8_t sram[CONFIG_SRAMSIZE] PG_ALIGN = {};
+#endif
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
-paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+uint8_t* guest_to_host(paddr_t paddr) {
+#ifdef CONFIG_YSYXSOC
+  if ((MROM_LEFT <= paddr) && (paddr < MROM_RIGHT)) return mrom + paddr - CONFIG_MROMBASE;
+  if ((SRAM_LEFT <= paddr) && (paddr < SRAM_RIGHT)) return sram + paddr - CONFIG_SRAMBASE;
+  return NULL;
+#else
+  return pmem + paddr - CONFIG_MBASE;
+#endif
+}
+
+paddr_t host_to_guest(uint8_t *haddr) {
+#ifdef CONFIG_YSYXSOC
+  if ((mrom <= haddr) && (haddr < mrom + CONFIG_MROMSIZE)) return haddr - mrom + CONFIG_MROMBASE;
+  if ((sram <= haddr) && (haddr < sram + CONFIG_SRAMSIZE)) return haddr - sram + CONFIG_SRAMBASE;
+  return 0;
+#else
+  return haddr - pmem + CONFIG_MBASE;
+#endif
+}
+
+// uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+// paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
@@ -43,11 +71,21 @@ static void out_of_bound(paddr_t addr) {
 
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
+#ifdef CONFIG_YSYXSOC
+  mrom = malloc(CONFIG_MROMSIZE);
+  sram = malloc(CONFIG_SRAMSIZE);
+  assert(mrom);
+  assert(sram);
+#endif
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
   IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+#ifdef CONFIG_YSYXSOC
+  Log("MROM area [" FMT_PADDR ", " FMT_PADDR "]", MROM_LEFT, MROM_RIGHT);
+  Log("SRAM area [" FMT_PADDR ", " FMT_PADDR "]", SRAM_LEFT, SRAM_RIGHT);
+#endif
+Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
 void print_paddr_read(paddr_t addr, int len)
