1778a34d142080f7a82f71ef1aa8ccd5a8d8fe14
tracer-ysyx <tracer@ysyx.org>
2025-12-27 18:45:45 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  18:45:45 up 49 min,  1 user,  load average: 2.64, 1.91, 1.50

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -16,11 +16,11 @@ extern char _pmem_start;
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
-extern char _data_start_load, _data_start, _data_end;
-extern char _bss_start, _bss_end;
+// extern char _data_start_load, _data_start, _data_end;
+// extern char _bss_start, _bss_end;
 
 void putch(char ch) {
-  asm volatile("sb %0, 0(%1)" : : "r"(ch), "r"(UART_BASE + UART_TX));
+  *(volatile char *)(UART_BASE + UART_TX) = ch;
 }
 
 void halt(int code) {
@@ -29,26 +29,7 @@ void halt(int code) {
   while (1);
 }
 
-void cp_data_to_sram() {
-  char *src = &_data_start_load;
-  char *dst = &_data_start;
-
-  while (dst < &_data_end) {
-    *dst = *src;
-    src++;
-    dst++;
-  }
-
-  char *p = &_bss_start;
-  while (p < &_bss_end)
-  {
-    *p = 0;
-    p++;
-  }
-}
-
 void _trm_init() {
-  cp_data_to_sram();
   int ret = main(mainargs);
   halt(ret);
 }
