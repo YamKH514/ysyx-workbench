93544e160fb890f57bef32b2d2a0a2701674f6e3
tracer-ysyx <tracer@ysyx.org>
2025-12-27 21:02:47 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  21:02:47 up  3:06,  1 user,  load average: 3.80, 3.76, 3.11

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -4,11 +4,4 @@
  * NPC Configuration Menu
  *
  */
-#define CONFIG_WATCHPOINT 1
 #define CONFIG_BATCH_MODE 1
-#define CONFIG_VCD_TRACE 1
-#define CONFIG_ITRACE 1
-#define CONFIG_TRACE_END 10000
-#define CONFIG_ITRACE_COND "true"
-#define CONFIG_TRACE_START 0
-#define CONFIG_TRACE 1
