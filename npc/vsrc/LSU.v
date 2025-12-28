c13b2f7dc4b9021f480c3c1f9b5b977be343be33
tracer-ysyx <tracer@ysyx.org>
2025-12-28 20:20:17 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  20:20:17 up 1 day,  2:23,  1 user,  load average: 1.14, 1.83, 2.11

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -1,7 +1,7 @@
 `include "common.vh"
 
 module LSU(
-    /* verilator lint_off UNUSEDSIGNAL */
+    
     input               clk,
     input               rst,
 
@@ -81,6 +81,10 @@ reg [2:0] state, next_state;
 reg         aw_handshake_r;
 reg [3:0]   rid_r;
 reg [31:0]  rdata_r;
+reg [31:0]  lsu_w_data_r;
+reg [31:0]  wdata_r;
+reg [3:0]   wmask_r;
+reg [3:0]   wstrb_r;
 reg [3:0]   bid_r;
 
 always @(posedge clk) begin
@@ -128,7 +132,6 @@ always @(posedge clk) begin
                     if (lsu_re_r) begin
                         arid_out    <= 4'b0;
                         araddr_out  <= lsu_r_addr_in;
-                        byte_off_r  <= lsu_r_addr_in[1:0];
                         arlen_out   <= 4'b0;
                         arsize_out  <= 3'b010;
                         arburst_out <= 2'b01;
@@ -140,10 +143,8 @@ always @(posedge clk) begin
                         awsize_out  <= 3'b010;
                         awburst_out <= 2'b01;
                         awvalid_out <= 1'b1;
-                        wdata_out   <= lsu_w_data_in;
-                        wstrb_out   <= lsu_w_mask_r;
-                        wlast_out   <= 1'b1;
-                        wvalid_out  <= 1'b1;
+                        lsu_w_data_r<= lsu_w_data_in;
+                        wmask_r     <= lsu_w_mask_r;
                     end
                     br_out      <= 1'b0;
                     bs_out      <= 1'b1;
@@ -179,6 +180,10 @@ always @(posedge clk) begin
                     awsize_out     <= 3'b0;
                     awburst_out    <= 2'b0;
                     awvalid_out    <= 1'b0;
+                    wdata_out      <= wdata_r;
+                    wstrb_out      <= wstrb_r;
+                    wlast_out      <= 1'b1;
+                    wvalid_out     <= 1'b1;
                 end
                 if (wvalid_out & wready_in) begin
                     wdata_out     <= 32'b0;
@@ -269,23 +274,37 @@ always @(*) begin
     endcase
 end
 
-reg     [1:0]   byte_off_r;
-wire    [7:0]   data_b;
-wire    [15:0]  data_h;
+reg     [1:0]   r_byte_off_r;
+reg     [1:0]   w_byte_off_r;
+wire    [7:0]   r_data_b;
+wire    [15:0]  r_data_h;
+wire    [7:0]   w_data_b;
+wire    [15:0]  w_data_h;
 
-// assign byte_off_r = lsu_r_addr_in[1:0];
+assign r_byte_off_r = lsu_r_addr_in[1:0];
 
-assign data_b = {8{byte_off_r == 2'b00}} & rdata_r[7:0]  |
-                {8{byte_off_r == 2'b01}} & rdata_r[15:8] |
-                {8{byte_off_r == 2'b10}} & rdata_r[23:16]|
-                {8{byte_off_r == 2'b11}} & rdata_r[31:24];
+assign r_data_b =   {8{r_byte_off_r == 2'b00}} & rdata_r[7:0]  |
+                    {8{r_byte_off_r == 2'b01}} & rdata_r[15:8] |
+                    {8{r_byte_off_r == 2'b10}} & rdata_r[23:16]|
+                    {8{r_byte_off_r == 2'b11}} & rdata_r[31:24];
 
-assign data_h = byte_off_r[1] == 1'b0 ? rdata_r[15:0] : rdata_r[31:16];
+assign r_data_h = r_byte_off_r[1] == 1'b0 ? rdata_r[15:0] : rdata_r[31:16];
 
-assign lsu_r_data_out = {32{lsu_r_func_r == `MEM_READ_FUNC_LBU}} & {24'b0, data_b[7:0]} |
-                        {32{lsu_r_func_r == `MEM_READ_FUNC_LB}}  & {{24{data_b[7]}}, data_b[7:0]} |
-                        {32{lsu_r_func_r == `MEM_READ_FUNC_LHU}} & {16'b0, data_h[15:0]} |
-                        {32{lsu_r_func_r == `MEM_READ_FUNC_LH}}  & {{16{data_h[15]}}, data_h[15:0]} |
+assign lsu_r_data_out = {32{lsu_r_func_r == `MEM_READ_FUNC_LBU}} & {24'b0, r_data_b[7:0]} |
+                        {32{lsu_r_func_r == `MEM_READ_FUNC_LB}}  & {{24{r_data_b[7]}}, r_data_b[7:0]} |
+                        {32{lsu_r_func_r == `MEM_READ_FUNC_LHU}} & {16'b0, r_data_h[15:0]} |
+                        {32{lsu_r_func_r == `MEM_READ_FUNC_LH}}  & {{16{r_data_h[15]}}, r_data_h[15:0]} |
                         {32{lsu_r_func_r == `MEM_READ_FUNC_LW}}  & rdata_r;
+
+assign w_byte_off_r = lsu_w_addr_in[1:0];
+assign w_data_b =   {8{wmask_r == 4'b0001}} & lsu_w_data_r[7:0]  |
+                    {8{wmask_r == 4'b0010}} & lsu_w_data_r[15:8] |
+                    {8{wmask_r == 4'b0100}} & lsu_w_data_r[23:16]|
+                    {8{wmask_r == 4'b1000}} & lsu_w_data_r[31:24];
+assign w_data_h =   {16{wmask_r == 4'b0011}} & lsu_w_data_r[15:0] |
+                    {16{wmask_r == 4'b0110}} & lsu_w_data_r[23:8] |
+                    {16{wmask_r == 4'b1100}} & lsu_w_data_r[31:16];
+assign wdata_r = ({24'b0, w_data_b} | {16'b0, w_data_h}) << (4 * w_byte_off_r);
+assign wstrb_r = wmask_r << w_byte_off_r;
 
 endmodule
