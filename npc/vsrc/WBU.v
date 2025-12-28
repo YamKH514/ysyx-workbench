b8eb83b078e5bed12069e00dcdc49ef815fba71e
tracer-ysyx <tracer@ysyx.org>
2025-12-28 14:11:59 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  14:11:59 up 20:15,  1 user,  load average: 2.52, 2.64, 2.64

--- npc/vsrc/WBU.v
+++ npc/vsrc/WBU.v
@@ -44,7 +44,6 @@ always @(posedge clk) begin
         case (state)
             S_IDLE: begin
                 wbu_to_lsu_ready_out <= 1'b0;
-                wbu_to_pc_valid_out <= 1'b0;
                 gpr_we_out <= 1'b0;
                 csr_w_ecall_out <= 1'b0;
                 csr_w_mret_out <= 1'b0;
