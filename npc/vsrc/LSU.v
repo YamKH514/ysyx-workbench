12f36af730d40eafb80c35c3dc56aa0a170eecd2
tracer-ysyx <tracer@ysyx.org>
2025-12-28 20:37:15 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  20:37:15 up 1 day,  2:40,  1 user,  load average: 1.52, 2.40, 2.45

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -277,14 +277,11 @@ reg     [31:0]  wdata_aligned;
 reg     [3:0]   wstrb_aligned;
 
 assign byte_off_r = lsu_r_addr_in[1:0];
-
 assign data_b = {8{byte_off_r == 2'b00}} & rdata_r[7:0]  |
                 {8{byte_off_r == 2'b01}} & rdata_r[15:8] |
                 {8{byte_off_r == 2'b10}} & rdata_r[23:16]|
                 {8{byte_off_r == 2'b11}} & rdata_r[31:24];
-
 assign data_h = byte_off_r[1] == 1'b0 ? rdata_r[15:0] : rdata_r[31:16];
-
 assign lsu_r_data_out = {32{lsu_r_func_r == `MEM_READ_FUNC_LBU}} & {24'b0, data_b[7:0]} |
                         {32{lsu_r_func_r == `MEM_READ_FUNC_LB}}  & {{24{data_b[7]}}, data_b[7:0]} |
                         {32{lsu_r_func_r == `MEM_READ_FUNC_LHU}} & {16'b0, data_h[15:0]} |
@@ -292,18 +289,19 @@ assign lsu_r_data_out = {32{lsu_r_func_r == `MEM_READ_FUNC_LBU}} & {24'b0, data_
                         {32{lsu_r_func_r == `MEM_READ_FUNC_LW}}  & rdata_r;
 
 assign waddr_aligned = {lsu_w_addr_in[31:2], 2'b00};
-
 assign w_byte_off = lsu_w_addr_in[1:0];
-
-always @(*) begin
-    case (w_byte_off)
-        2'b00: wdata_aligned = lsu_w_data_in;
-        2'b01: wdata_aligned = lsu_w_data_in << 8;
-        2'b10: wdata_aligned = lsu_w_data_in << 16;
-        2'b11: wdata_aligned = lsu_w_data_in << 24;
-    endcase
-end
-
+assign wdata_aligned =  {32{w_byte_off == 2'b00}} & lsu_w_data_in      |
+                        {32{w_byte_off == 2'b01}} & lsu_w_data_in << 8 |
+                        {32{w_byte_off == 2'b10}} & lsu_w_data_in << 16|
+                        {32{w_byte_off == 2'b11}} & lsu_w_data_in << 24;
+// always @(*) begin
+//     case (w_byte_off)
+//         2'b00: wdata_aligned = lsu_w_data_in;
+//         2'b01: wdata_aligned = lsu_w_data_in << 8;
+//         2'b10: wdata_aligned = lsu_w_data_in << 16;
+//         2'b11: wdata_aligned = lsu_w_data_in << 24;
+//     endcase
+// end
 assign wstrb_aligned = lsu_w_mask_r << w_byte_off;
 
 endmodule
