fc5509534e64cd4b4a14241fbdf5bc6c57f35ecd
tracer-ysyx <tracer@ysyx.org>
2025-12-30 22:39:06 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  22:39:06 up 3 days,  4:42,  1 user,  load average: 0.55, 0.58, 0.60

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -6,26 +6,24 @@
 #define UART_TX 0x0
 
 extern char _heap_start;
-extern char _heap_end;
+#define _heap_end 0x0f001fff
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_SIZE 0x1000
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 #define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
-Area heap = RANGE(&_heap_start, &_heap_end);
+Area heap = RANGE(&_heap_start, _heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
-
-extern char _data_start[], _data_load_start[], _data_load_end;
 
 void putch(char ch) {
   *(volatile char *)(UART_BASE + UART_TX) = ch;
 }
 
-void bootloader() {
-  size_t load_size = (uintptr_t)&_data_load_end - (uintptr_t)_data_load_start;
-  memcpy(_data_start, _data_load_start, load_size);
+static void bootloader() {
+  extern char _data_start[], _load_data_start[], _load_data_size[];
+    memcpy(_data_start, _load_data_start, (size_t)_load_data_size);
 }
 
 void halt(int code) {
