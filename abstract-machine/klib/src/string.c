6d6d1e5d92ee37ddef91f6f95a14d75ae07f6c90
tracer-ysyx <tracer@ysyx.org>
2026-01-04 14:34:55 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  14:34:55 up 7 days, 20:38,  1 user,  load average: 0.56, 0.75, 0.85

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -51,16 +51,19 @@ char *strcat(char *dst, const char *src) {
 int strcmp(const char *s1, const char *s2) {
   while (*s1 != '\0' && *s2 != '\0')
   {
-    if(*s1 != *s2)
+    unsigned char c1 = (unsigned char) *s1;
+    unsigned char c2 = (unsigned char) *s2;
+    if(c1 != c2)
     {
-      return (*s1 < *s2) ? -1 : 1;
+      return (c1 < c2) ? -1 : 1;
     }
     s1 ++;
     s2 ++;
   }
-  if(*s1 == *s2) return 0;
-
-  return (*s1 < *s2) ? -1 : 1;
+  unsigned char c1 = (unsigned char) *s1;
+  unsigned char c2 = (unsigned char) *s2;
+  if(c1 == c2) return 0;
+  return (c1 < c2) ? -1 : 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
