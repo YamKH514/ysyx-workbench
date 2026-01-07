d52dd21242e1f6aa9f91b5e075de6ce27fc3e86b
tracer-ysyx <tracer@ysyx.org>
2026-01-07 16:48:05 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  16:48:05 up 10 days, 22:51,  1 user,  load average: 0.71, 0.71, 0.65

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -4,6 +4,16 @@
 
 #define UART_BASE 0x10000000L
 #define UART_TX 0x0
+#define UART_RX 0x0
+#define UART_IER 0x1
+#define UART_IIR 0x2
+#define UART_FCR 0x2
+#define UART_LCR 0x3
+#define UART_MCR 0x4
+#define UART_LSR 0x5
+#define UART_MSR 0x6
+#define UART_LSB 0x0
+#define UART_MSB 0x1
 
 extern char _heap_start;
 #define _heap_end 0x0f001fff
@@ -17,6 +27,18 @@ extern char _pmem_start;
 Area heap = RANGE(&_heap_start, _heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
+static void uart_init() {
+  *(volatile char *)(UART_BASE + UART_FCR) = 0x06; // Clear & reset RX&TX FIFOs
+
+  // Set Divisor Latch register
+  *(volatile char *)(UART_BASE + UART_LCR) = 0x80;
+  *(volatile char *)(UART_BASE + UART_MSB) = 0x00;
+  *(volatile char *)(UART_BASE + UART_LSB) = 0x01;
+
+  *(volatile char *)(UART_BASE + UART_LCR) = 0x03; // Set LCR 8 bits of data, np parity and 1 stop bit
+  *(volatile char *)(UART_BASE + UART_IER) = 0x00; // Disable all interrupts
+}
+
 void putch(char ch) {
   *(volatile char *)(UART_BASE + UART_TX) = ch;
 }
@@ -25,7 +47,7 @@ extern char data_start [];
 extern char data_size [];
 extern char data_load_start [];
 
-void bootloader()
+static void bootloader()
 {
   memcpy(data_start, data_load_start, (size_t) data_size);
 }
@@ -37,6 +59,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+  uart_init();
   bootloader();
   int ret = main(mainargs);
   halt(ret);
