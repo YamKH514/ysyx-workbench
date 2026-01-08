6395a1acf6afc2bd147c56e6c02bf685c6c9ec0d
tracer-ysyx <tracer@ysyx.org>
2026-01-08 16:52:10 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  16:52:10 up 11 days, 22:55,  1 user,  load average: 0.40, 0.25, 0.35

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -10,7 +10,6 @@
 #define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
 #define RTC_ADDR    (DEVICE_BASE + 0x0000048)
 
-#define FLASH_BASE 0x30000000
 static uint8_t flash_data[10] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99};
 
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
@@ -89,7 +88,7 @@ extern "C" void mrom_read(int32_t addr, int32_t *data)
 extern "C" void flash_read(int32_t addr, int32_t *data)
 {
     printf("FLASH READ ADDR=0x%08x\n", addr);
-    *data = (int32_t)flash_data[(addr - FLASH_BASE)];
+    *data = (int32_t)flash_data[addr];
 }
 
 extern "C" void paddr_write(uint32_t waddr, uint32_t wdata, uint8_t wmask)
