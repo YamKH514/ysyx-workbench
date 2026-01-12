ca36cdcb8019779e7b5ce51717cd2f40eb122607
tracer-ysyx <tracer@ysyx.org>
2026-01-12 21:57:25 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  21:57:25 up  2:07,  1 user,  load average: 2.31, 1.38, 1.10

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -22,14 +22,14 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 static uint8_t psram[CONFIG_PSRAM_MSIZE] PG_ALIGN = {};
 
 uint8_t *guest_to_host(uint32_t paddr) {
-    if ((CONFIG_MBASE <= paddr) && (paddr <= CONFIG_MSIZE)) return pmem + paddr - CONFIG_MBASE;
-    else if ((CONFIG_PSRAM_MBASE <= paddr) && (paddr <= CONFIG_PSRAM_MSIZE)) return psram + paddr - CONFIG_PSRAM_MBASE;
+    if ((CONFIG_MBASE <= paddr) && (paddr <= CONFIG_MBASE + CONFIG_MSIZE)) return pmem + paddr - CONFIG_MBASE;
+    else if ((CONFIG_PSRAM_MBASE <= paddr) && (paddr <= CONFIG_PSRAM_MBASE + CONFIG_PSRAM_MSIZE)) return psram + paddr - CONFIG_PSRAM_MBASE;
     assert(0);
 }
 
 uint32_t host_to_guest(uint8_t *haddr) {
-    if ((CONFIG_MBASE <= (uintptr_t)haddr) && ((uintptr_t)haddr <= CONFIG_MSIZE)) return haddr - pmem + CONFIG_MBASE;
-    else if ((CONFIG_PSRAM_MBASE <= (uintptr_t)haddr) && ((uintptr_t)haddr <= CONFIG_PSRAM_MSIZE)) return haddr - psram + CONFIG_PSRAM_MBASE;
+    if ((CONFIG_MBASE <= (uintptr_t)haddr) && ((uintptr_t)haddr <=  CONFIG_MBASE + CONFIG_MSIZE)) return haddr - pmem + CONFIG_MBASE;
+    else if ((CONFIG_PSRAM_MBASE <= (uintptr_t)haddr) && ((uintptr_t)haddr <= CONFIG_PSRAM_MBASE + CONFIG_PSRAM_MSIZE)) return haddr - psram + CONFIG_PSRAM_MBASE;
     assert(0);
 }
 
