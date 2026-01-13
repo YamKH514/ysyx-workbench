5b846312740e4b6dd7e5dc9effc6e1f65e9255a3
tracer-ysyx <tracer@ysyx.org>
2026-01-13 22:08:49 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  22:08:49 up  9:01,  1 user,  load average: 0.52, 1.33, 1.22

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -70,11 +70,12 @@ void _ssbl() {
   extern char rodata_size [];
   extern char rodata_load_start [];
   extern char data_start [];
+  extern char data_end [];
   extern char data_size [];
   extern char data_load_start [];
   MEMCOPY(text_start, text_load_start, text_size);
   MEMCOPY(rodata_start, rodata_load_start, rodata_size);
-  MEMCOPY(data_start, data_load_start, data_size);
+  if (data_end - data_start) MEMCOPY(data_start, data_load_start, data_size);
   _trm_init();
 }
 
