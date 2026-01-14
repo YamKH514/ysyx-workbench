7999d69742d8ae4fe784d099bba3143b35d0dcd9
tracer-ysyx <tracer@ysyx.org>
2026-01-14 17:11:59 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  17:11:59 up 26 min,  1 user,  load average: 0.09, 0.27, 0.40

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -74,14 +74,20 @@ void _ssbl() {
   extern char data_end [];
   extern char data_size [];
   extern char data_load_start [];
-  // extern char data_extra_start [];
-  // extern char data_extra_end [];
-  // extern char data_extra_size [];
-  // extern char data_extra_load_start [];
+  extern char data_extra_start [];
+  extern char data_extra_end [];
+  extern char data_extra_size [];
+  extern char data_extra_load_start [];
+  extern char _bss_start [];
+  extern char _bss_end [];
+  extern char _bss_extra_start [];
+  extern char _bss_extra_end [];
   MEMCOPY(text_start, text_load_start, text_size);
   if (rodata_end - rodata_start) MEMCOPY(rodata_start, rodata_load_start, rodata_size);
   if (data_end - data_start) MEMCOPY(data_start, data_load_start, data_size);
-  // if (data_extra_end - data_extra_start) MEMCOPY(data_extra_start, data_extra_load_start, data_extra_size);
+  if (data_extra_end - data_extra_start) MEMCOPY(data_extra_start, data_extra_load_start, data_extra_size);
+  if (_bss_end - _bss_start) MEMSETZ(_bss_start, _bss_end);
+  if (_bss_extra_end - _bss_extra_start) MEMSETZ(_bss_extra_start, _bss_extra_end);
   _trm_init();
 }
 
