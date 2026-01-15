e76d19675e63a3ff86b30b83857db98b9bb97209
tracer-ysyx <tracer@ysyx.org>
2026-01-15 16:57:40 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  16:57:40 up 49 min,  1 user,  load average: 1.05, 1.20, 1.21

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -145,7 +145,7 @@ static void execute(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVcdC
         running_cycle ++;
         g_nr_guest_inst++;
         // if ((running_cycle % 1000000) == 0) printf("NPC has been runned %llu cycle\n", running_cycle);
-        if ((contextp->gotFinish()) || (npc_state.state == NPC_ABORT))
+        if ((contextp->gotFinish()) || (npc_state.state == NPC_ABORT) || (npc_state.state == NPC_STOP))
         {
             npc_state.halt_pc = cpu.pc;
             npc_state.halt_ret = cpu.gpr[10];
@@ -168,7 +168,7 @@ void npc_exec(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVcdC *tfp,
         return;
     } else
     {
-        npc_state.state == NPC_RUNNING
+        npc_state.state == NPC_RUNNING;
     }
     execute(top, contextp, tfp, n);
 }
