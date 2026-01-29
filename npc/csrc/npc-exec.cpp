92f0775a8cbcabcd5e39be7abfe012e822acf460
tracer-ysyx <tracer@ysyx.org>
2026-01-29 09:49:38 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  09:49:38 up 2 days, 13:50,  1 user,  load average: 0.80, 1.14, 1.44

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -55,8 +55,28 @@ static void exec_once(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVc
         pc = S_CPU(pc);
         inst_end = true;
     }
+    if ((!S_CPU(wbu_to_pc_valid)) & (!S_CPU(pc_to_wbu_ready)) & inst_end)
+    {
+        inst_end = false;
+        cpu.pc = S_CPU(pc);
+        cpu.npc = S_CPU(npc);
+        svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu.u_GPR.u_RegisterFile"));
+        get_gpr(cpu.gpr);
+        svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu.u_CSR"));
+        get_csr((int *)(&cpu.csr));
+#ifdef CONFIG_WATCHPOINT
+        bool changed = wp_scan();
+        if (changed)
+        {
+            npc_state.state = NPC_STOP;
+        }
+#endif
+#ifdef CONFIG_DIFFTEST
+        difftest_step(pc);
+#endif
+    }
 
-    if (in_pmem(pc))
+    if (in_pmem(pc) & (!S_CPU(wbu_to_pc_valid)) & (!S_CPU(pc_to_wbu_ready)) & inst_end)
     {
         // 反汇编 itrace
         char *p = logbuf;
@@ -80,6 +100,7 @@ static void exec_once(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVc
 
         disassemble(p, logbuf + sizeof(logbuf) - p, pc, inst, ilen);
 #endif
+        trace_and_difftest(top, logbuf);
 
         // 函数调用 ftrace
         uint8_t opcode = BITS(inst_val, 6, 0);
@@ -113,28 +134,6 @@ static void exec_once(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVc
             }
 #endif
         }
-    }
-
-    if ((!S_CPU(wbu_to_pc_valid)) & (!S_CPU(pc_to_wbu_ready)) & inst_end)
-    {
-        inst_end = false;
-        cpu.pc = S_CPU(pc);
-        cpu.npc = S_CPU(npc);
-        svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu.u_GPR.u_RegisterFile"));
-        get_gpr(cpu.gpr);
-        svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu.u_CSR"));
-        get_csr((int *)(&cpu.csr));
-#ifdef CONFIG_WATCHPOINT
-        bool changed = wp_scan();
-        if (changed)
-        {
-            npc_state.state = NPC_STOP;
-        }
-#endif
-#ifdef CONFIG_DIFFTEST
-        difftest_step(pc);
-#endif
-        trace_and_difftest(top, logbuf);
     }
 }
 
