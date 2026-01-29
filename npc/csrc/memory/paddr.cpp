a95b14a3c2031be84cba5cedf3f0a014fa2db778
tracer-ysyx <tracer@ysyx.org>
2026-01-29 16:50:10 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Fri Nov 14 21:44:46 UTC 2025 x86_64 GNU/Linux  16:50:10 up 2 days, 20:50,  1 user,  load average: 3.36, 2.77, 2.63

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -122,46 +122,44 @@ extern "C" void psram_write(int32_t addr, int32_t data, int32_t mask) {
     return;
 }
 
-// extern "C" void sdram_read(int32_t addr, int32_t *data) {
-//     uint32_t raddr = CONFIG_SDRAMBASE + (uint32_t)addr;
-//     *data = pmem_read(raddr, 2);
-//     printf("sdram_read: raddr: 0x%08x, data: 0x%08x\n", raddr, *data);
-//     return;
-// }
+extern "C" int32_t sdram_read(int32_t addr) {
+    uint32_t raddr = CONFIG_SDRAMBASE + (uint32_t)addr;
+    uint32_t rdata = pmem_read(raddr, 2);
+    printf("sdram_read: raddr: 0x%08x, data: 0x%08x\n", raddr, rdata);
+    return (int32_t)rdata;
+}
 
-// extern "C" void sdram_write(int32_t addr, int32_t data, int32_t mask) {
-//     printf("sdram_write: addr: 0x%08x, data: 0x%08x, mask: 0x%08x\n", addr, data, mask);
-//     uint32_t waddr = CONFIG_SDRAMBASE + (uint32_t)addr;
-//     uint32_t wdata;
-//     int len;
-//     switch (mask) {
-//         case 0x3:   // 16bit均无效
-//             return;
-//         case 0x2:   // 低8bit有效
-//             len = 1;
-//             wdata = (data & 0x00FF);
-//             break;
-//         case 0x1:   // 高8bit有效
-//             len = 1;
-//             waddr += 1;
-//             wdata = (data & 0xFF00) >> 8;
-//             break;
-//         case 0x0:   // 16bit均有效
-//             len = 2;
-//             wdata = data;
-//             break;
-//     }
-//     printf("sdram_write: waddr: 0x%08x, len: %08x, wdata: 0x%08x\n", waddr, len, wdata);
-//     pmem_write(waddr, len, wdata);
-//     return;
-// }
+extern "C" void sdram_write(int32_t addr, int32_t data, int32_t dqm) {
+    uint32_t waddr = CONFIG_SDRAMBASE + (uint32_t)addr;
+    uint32_t wdata;
+    int len;
+    switch (dqm) {
+        case 0x3:   // 16bit均无效
+            return;
+        case 0x2:   // 低8bit有效
+            len = 1;
+            wdata = (data & 0x00FF);
+            break;
+        case 0x1:   // 高8bit有效
+            len = 1;
+            waddr += 1;
+            wdata = (data & 0xFF00) >> 8;
+            break;
+        case 0x0:   // 16bit均有效
+            len = 2;
+            wdata = data;
+            break;
+    }
+    pmem_write(waddr, len, wdata);
+    return;
+}
 
 extern "C" uint32_t paddr_read(uint32_t raddr)
 {
     uint32_t rdata = 0;
     if ((PMEM_LEFT <= raddr) & (raddr < PMEM_RIGHT)) flash_read(raddr - PMEM_LEFT, (int32_t *)&rdata);
     else if ((PSRAM_LEFT <= raddr) & (raddr < PSRAM_RIGHT)) psram_read(raddr - PSRAM_LEFT, (int32_t *)&rdata);
-    // else if ((SDRAM_LEFT <= raddr) & (raddr < SDRAM_RIGHT)) sdram_read(raddr - SDRAM_LEFT, (int32_t *)&rdata);
+    else if ((SDRAM_LEFT <= raddr) & (raddr < SDRAM_RIGHT)) sdram_read(raddr - SDRAM_LEFT, (int32_t *)&rdata);
     else assert(0);
     return rdata;
 }
