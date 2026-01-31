f670f7e618b31cbf7e3ecc083ad58232b75e418f
tracer-ysyx <tracer@ysyx.org>
2026-01-31 16:40:23 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  16:40:23 up  6:05,  1 user,  load average: 1.05, 1.38, 1.16

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -44,10 +44,6 @@ int main(int argc, char *argv[])
         npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
         npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)), npc_state.halt_pc);
 
-#ifdef CONFIG_NVBOARD
-    nvboard_quit();
-#endif
-
     tfp->close();
     top->final();
 
--- npc/csrc/state.cpp
+++ npc/csrc/state.cpp
@@ -1,9 +1,15 @@
 #include "utils.h"
+#ifdef CONFIG_NVBOARD
+#include <nvboard.h>
+#endif
 
 NPCState npc_state = {.state = NPC_STOP, .halt_pc = 0, .halt_ret = -1, .inited = false};
 
 int is_exit_status_bad() {
   int good = (npc_state.state == NPC_END && npc_state.halt_ret == 0) ||
     (npc_state.state == NPC_QUIT);
+#ifdef CONFIG_NVBOARD
+  nvboard_quit();
+#endif
   return !good;
 }
