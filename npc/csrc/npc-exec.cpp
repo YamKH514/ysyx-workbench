0fc989020aab3f5292086851a7bc2dec83209846
tracer-ysyx <tracer@ysyx.org>
2026-02-01 20:44:20 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  20:44:20 up 1 day, 10:09,  1 user,  load average: 2.83, 2.13, 1.66

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -49,7 +49,7 @@ static void exec_once(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVc
 {
     char logbuf[128];
 
-    if (!npc_state.inited) cpu_reset(10, top, contextp, tfp);
+    if (!npc_state.inited) cpu_reset(20, top, contextp, tfp);
 
     cpu_single_cycle(top, contextp, tfp);
 
