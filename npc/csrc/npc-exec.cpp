fde2616679e65ec5b0805fb2e094581de0609f6e
tracer-ysyx <tracer@ysyx.org>
2026-02-02 14:57:59 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  14:57:59 up 2 days,  4:23,  1 user,  load average: 1.12, 0.69, 0.63

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -57,10 +57,12 @@ static void exec_once(VysyxSoCFull *top, VerilatedContext *contextp, VerilatedVc
     if (S_CPU(ifu_to_idu_valid) & S_CPU(idu_to_ifu_ready)) inst_num ++;
 
     // Perf CNT
-    perf_cnt_module_add(S_CPU(ifu_to_idu_valid) & S_CPU(idu_to_ifu_ready), IFU);
-    perf_cnt_module_add(S_CPU(idu_to_exu_valid) & S_CPU(exu_to_idu_ready), EXU);
-    perf_cnt_module_add(S_CPU(exu_to_lsu_valid) & S_CPU(lsu_to_exu_ready), LSU);
-    perf_cnt_inst_add(S_CPU(idu_to_exu_valid) & S_CPU(exu_to_idu_ready), (INST_TYPE_ENUM)S_CPU(inst_type));
+    if (!top->reset) {
+        perf_cnt_module_add(S_CPU( pc_to_ifu_valid) & S_CPU( ifu_to_pc_ready), IFU);
+        perf_cnt_module_add(S_CPU(idu_to_exu_valid) & S_CPU(exu_to_idu_ready), EXU);
+        perf_cnt_module_add(S_CPU(exu_to_lsu_valid) & S_CPU(lsu_to_exu_ready), LSU);
+        perf_cnt_inst_add(S_CPU(idu_to_exu_valid) & S_CPU(exu_to_idu_ready), (INST_TYPE_ENUM)S_CPU(inst_type));
+    }
 
     if ((S_CPU(wbu_to_pc_valid)) & (S_CPU(pc_to_wbu_ready))) 
     {
