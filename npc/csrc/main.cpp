766361c7ab98cce6beaf97e67eb933ed33d6c33c
tracer-ysyx <tracer@ysyx.org>
2026-02-02 17:58:17 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  17:58:17 up 2 days,  7:23,  1 user,  load average: 1.75, 2.05, 1.41

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -46,10 +46,9 @@ int main(int argc, char *argv[])
     perf_cnt.print_module_called(IFU);
     perf_cnt.print_module_called(EXU);
     perf_cnt.print_module_called(LSU);
-    // Log("IFU called %llu, EXU called %llu, LSU called %llu",
-    //     perf_cnt.module(IFU), perf_cnt.module(EXU), perf_cnt.module(LSU));
-    Log("type_i = %f, type_s = %f, type_b = %f, type_u = %f, type_j = %f, type_r = %f",
-        perf_cnt.inst_proportion(i), perf_cnt.inst_proportion(s), perf_cnt.inst_proportion(b), perf_cnt.inst_proportion(u), perf_cnt.inst_proportion(j), perf_cnt.inst_proportion(r));
+    perf_cnt.print_inst_info(i);
+    // Log("type_i = %f, type_s = %f, type_b = %f, type_u = %f, type_j = %f, type_r = %f",
+    //     perf_cnt.inst_proportion(i), perf_cnt.inst_proportion(s), perf_cnt.inst_proportion(b), perf_cnt.inst_proportion(u), perf_cnt.inst_proportion(j), perf_cnt.inst_proportion(r));
     Log("npc: %s at pc = 0x%08x", (
         npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
         npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)), npc_state.halt_pc);
--- npc/csrc/perf-cnt.cpp
+++ npc/csrc/perf-cnt.cpp
@@ -8,9 +8,17 @@ static const char *module_lut[__MODULE_CNT_NUM] = {
     [LSU] = "LSU",
 };
 
+static const char *inst_type_lut[__INST_TYPE_NUM] = {
+    [i] = "TYPE_I",
+    [s] = "TYPE_S",
+    [b] = "TYPE_B",
+    [u] = "TYPE_U",
+    [j] = "TYPE_J",
+    [r] = "TYPE_R",
+};
+
 void Perf_cnt::module_add(MODULE_ENUM module) {
     this->__module[module] ++;
-    this->__total_module ++;
 }
 
 void Perf_cnt::inst_add(INST_TYPE_ENUM inst_type, int num) {
@@ -32,12 +40,14 @@ unsigned long long Perf_cnt::module(MODULE_ENUM module) {
     return this->__module[module];
 }
 
-unsigned long long Perf_cnt::total_module() {
-    return this->__total_module;
-}
-
 void Perf_cnt::print_module_called(MODULE_ENUM module) {
     Log("%s called %llu", module_lut[module], this->__module[module]);
+}
+
+void Perf_cnt::print_inst_info(INST_TYPE_ENUM inst_type) {
+    Log("%s inst was executed %d times, which is approximately %f of all instructions,\n    and on average, it required %llu cycles to execute.",
+        inst_type_lut[inst_type], (double)(this->__inst[inst_type].num)/(double)(this->__total_inst.num),
+        this->__inst[inst_type].cyc/this->__inst[inst_type].num);
 }
 
 Perf_cnt perf_cnt;
