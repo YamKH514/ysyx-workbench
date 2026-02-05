3220cceaec6f82d5587782d1ef0ba0f746aadaad
tracer-ysyx <tracer@ysyx.org>
2026-02-05 19:59:46 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  19:59:46 up  9:48,  1 user,  load average: 2.52, 1.81, 1.48

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -26,7 +26,11 @@ void init_difftest(char *ref_so_file, long img_size, int port)
   assert(ref_so_file != NULL);
 
   CPU_state cpu_init = cpu;
+#ifdef PLATFORM_YSYXSOC
   cpu_init.pc = CONFIG_MBASE;
+#else
+  cpu_init.pc = CONFIG_NPC_PMEMBASE;
+#endif
   cpu_init.csr.mstatus = 0x00001800;
 
   void *handle;
