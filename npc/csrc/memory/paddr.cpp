4331000e913691fc8b409d2ae3385be4fc246a48
tracer-ysyx <tracer@ysyx.org>
2026-02-05 16:40:04 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  16:40:04 up  6:28,  1 user,  load average: 1.31, 1.44, 1.56

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -181,7 +181,7 @@ uint32_t paddr_read(uint32_t raddr)
     else assert(0);
     return rdata;
 #else
-    if ((PMEM_LEFT <= raddr) & (raddr < PMEM_RIGHT)) return pmem_read(raddr - PMEM_LEFT, 4);
+    if ((PMEM_LEFT <= raddr) & (raddr < PMEM_RIGHT)) return pmem_read(raddr, 4);
     printf("npc raddr: 0x%08x\n", raddr);
 #endif
     assert(0);
