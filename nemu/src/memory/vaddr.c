095fb32aadfce538275ab79f10728f515f495a3e
tracer-ysyx <tracer@ysyx.org>
2026-02-07 11:39:58 +0800

    > compile NEMU ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  11:39:58 up 2 days,  1:28,  1 user,  load average: 0.69, 0.81, 0.96

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -28,5 +28,8 @@ word_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+  if (addr == UART_LEFT) {
+    printf("%c", (char)data);
+  }
   paddr_write(addr, len, data);
 }
