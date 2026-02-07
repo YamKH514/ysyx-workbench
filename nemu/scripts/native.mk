f6e529dcb681335ca86a85e91965f2b0976b8e7d
tracer-ysyx <tracer@ysyx.org>
2026-02-07 13:52:04 +0800

    > compile NEMU ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  13:52:04 up 2 days,  3:40,  1 user,  load average: 0.73, 1.08, 1.25

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,18 +26,23 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-MAINARGS_PLACEHOLDER = test
-override ARGS += -DMAINARGS_PLACEHOLDER=\""$(MAINARGS_PLACEHOLDER)"\"
 
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
+
+MAINARGS_MAX_LEN = 64
+MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
+insert-arg:
+	@python3 $(AM_HOME)/tools/insert-arg.py $(IMG) $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
+
+run-soc: insert-arg run
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
