99bfb3dbf0d13f2f9d3d58ed2bedcedf458627f6
tracer-ysyx <tracer@ysyx.org>
2026-02-08 15:26:12 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  15:26:12 up 3 days,  5:14,  1 user,  load average: 2.09, 0.91, 0.79

--- npc/vsrc/ICache.v
+++ npc/vsrc/ICache.v
@@ -2,13 +2,13 @@ module ICache(
     input               clk,
     input               rst,
 
-    input [31-CACHE_M:0]araddr_in,
+    input       [29:0]  araddr_in,
     input               arvalid_in,
     output  reg [31:0]  rdata_out,
     output  reg         rvalid_out,
     output  reg         rready_out,
 
-    input [31-CACHE_M:0]awaddr_in,
+    input       [29:0]  awaddr_in,
     input               awvalid_in,
     input       [31:0]  wdata_in
 );
@@ -17,6 +17,7 @@ parameter  CACHE_M = 2;  /* 2^m Byte, default every cache line has 4Byte size */
 parameter  CACHE_N = 4;  /* CACHELINE_N = 2^n, default ICache has 16 cache line */
 localparam CACHELINE_W = 8 * (2 ** CACHE_M);
 localparam CACHELINE_N = 2 ** CACHE_N;
+localparam WORD_OFF_W = (CACHE_M > 2) ? (CACHE_M - 2) : 0;
 
 reg  [31-CACHE_M-CACHE_N:0] cache_tag  [CACHELINE_N];
 reg  [CACHELINE_W-1:0]      cache_data [CACHELINE_N];
@@ -25,13 +26,27 @@ wire [31-CACHE_M-CACHE_N:0] r_tag;
 wire [CACHE_N-1:0]          r_index;
 wire [31-CACHE_M-CACHE_N:0] w_tag;
 wire [CACHE_N-1:0]          w_index;
+wire [7:0]                  word_off;
 
-reg [31-CACHE_M:0] raddr_r;
+reg [29:0] raddr_r;
 
-assign {r_tag, r_index} = raddr_r;
-assign rdata_out = cache_data[r_index];
-assign rvalid_out = (cache_tag[r_index] == r_tag) && cache_valid[r_index];
-assign {w_tag, w_index} = awaddr_in;
+generate
+    if (WORD_OFF_W == 0) begin : GEN_NO_WORD_OFF
+        assign {r_tag, r_index} = raddr_r;
+        assign rdata_out = cache_data[r_index];
+        assign rvalid_out = (cache_tag[r_index] == r_tag) && cache_valid[r_index];
+        assign {w_tag, w_index} = awaddr_in;
+        assign word_off = 0;
+    end else begin : GEN_HAS_WORD_OFF
+        wire [WORD_OFF_W-1:0]       r_word_off;
+        wire [WORD_OFF_W-1:0]       w_word_off;
+        assign {r_tag, r_index, r_word_off} = raddr_r;
+        assign rdata_out = cache_data[r_index][32*r_word_off +: 32];
+        assign rvalid_out = (cache_tag[r_index] == r_tag) && cache_valid[r_index];
+        assign {w_tag, w_index, w_word_off} = awaddr_in;
+        assign word_off = {{(8-WORD_OFF_W){0}}, w_word_off};
+    end
+endgenerate
 
 always @(posedge clk) begin
     if (rst) begin
@@ -50,7 +65,7 @@ end
 always @(posedge clk) begin
     if (!rst && awvalid_in) begin
         cache_tag[w_index]   <= w_tag;
-        cache_data[w_index]  <= wdata_in;
+        cache_data[w_index][32*word_off +: 32]  <= wdata_in;
         cache_valid[w_index] <= 'd1;
     end
 end
--- npc/vsrc/Xbar.v
+++ npc/vsrc/Xbar.v
@@ -96,100 +96,135 @@ module Xbar(
     output              s1_bready
 );
 
-reg cur_slave;
+reg cur_slave_r;
+reg cur_slave_w;
 
 localparam S_W = 2;
 localparam S_IDLE = 2'd0;
 localparam S_BUSY = 2'd1;
 
-reg [S_W-1:0] state;
+reg [S_W-1:0] state_r;
+reg [S_W-1:0] state_w;
 
+// Read
 always @(posedge clk) begin
     if (rst) begin
-        state <= S_IDLE;
-        cur_slave <= 0;
+        state_r <= S_IDLE;
+        cur_slave_r <= 0;
     end else begin
-        case (state)
+        case (state_r)
             S_IDLE: begin
-                if (m_awvalid | m_arvalid) begin
-                    state <= S_BUSY;
-                    cur_slave <= m_awvalid ?m_awaddr == `RTC_ADDR:
-                                            m_araddr == `RTC_ADDR;
+                if (m_arvalid) begin
+                    state_r <= S_BUSY;
+                    cur_slave_r <= m_araddr == `RTC_ADDR;
                 end else begin
-                    state <= state;
-                    cur_slave <= cur_slave;
+                    state_r <= state_r;
+                    cur_slave_r <= cur_slave_r;
                 end
             end
             S_BUSY: begin
-                if ((m_bvalid && m_bready) | (m_rvalid && m_rready)) begin
-                    state <= S_IDLE;
-                    cur_slave <= 0;
+                if (m_rvalid && m_rready) begin
+                    state_r <= S_IDLE;
+                    cur_slave_r <= 0;
                 end else begin
-                    state <= state;
-                    cur_slave <= cur_slave;
+                    state_r <= state_r;
+                    cur_slave_r <= cur_slave_r;
                 end
             end
             default: begin
-                state <= S_IDLE;
-                cur_slave <= 0;
+                state_r <= S_IDLE;
+                cur_slave_r <= 0;
             end
         endcase
     end
 end
 
-assign s0_arid    = ({4{(cur_slave == 0)}} & m_arid) & {4{(state == S_BUSY)}};
-assign s1_arid    = ({4{(cur_slave == 1)}} & m_arid) & {4{(state == S_BUSY)}};
-assign s0_araddr  = ({32{(cur_slave == 0)}} & m_araddr) & {32{(state == S_BUSY)}};
-assign s1_araddr  = ({32{(cur_slave == 1)}} & m_araddr) & {32{(state == S_BUSY)}};
-assign s0_arlen   = ({4{(cur_slave == 0)}} & m_arlen) & {4{(state == S_BUSY)}};
-assign s1_arlen   = ({4{(cur_slave == 1)}} & m_arlen) & {4{(state == S_BUSY)}};
-assign s0_arsize  = ({3{(cur_slave == 0)}} & m_arsize) & {3{(state == S_BUSY)}};
-assign s1_arsize  = ({3{(cur_slave == 1)}} & m_arsize) & {3{(state == S_BUSY)}};
-assign s0_arburst = ({2{(cur_slave == 0)}} & m_arburst) & {2{(state == S_BUSY)}};
-assign s1_arburst = ({2{(cur_slave == 1)}} & m_arburst) & {2{(state == S_BUSY)}};
-assign s0_arvalid = ((cur_slave == 0) & m_arvalid) & (state == S_BUSY);
-assign s1_arvalid = ((cur_slave == 1) & m_arvalid) & (state == S_BUSY);
+// Write
+always @(posedge clk) begin
+    if (rst) begin
+        state_w <= S_IDLE;
+        cur_slave_w <= 0;
+    end else begin
+        case (state_w)
+            S_IDLE: begin
+                if (m_awvalid) begin
+                    state_w <= S_BUSY;
+                    cur_slave_w <= m_awaddr == `RTC_ADDR;
+                end else begin
+                    state_w <= state_w;
+                    cur_slave_w <= cur_slave_w;
+                end
+            end
+            S_BUSY: begin
+                if (m_bvalid && m_bready) begin
+                    state_w <= S_IDLE;
+                    cur_slave_w <= 0;
+                end else begin
+                    state_w <= state_w;
+                    cur_slave_w <= cur_slave_w;
+                end
+            end
+            default: begin
+                state_w <= S_IDLE;
+                cur_slave_w <= 0;
+            end
+        endcase
+    end
+end
 
-assign m_arready =  (state == S_BUSY) & ((cur_slave == 0) ? s0_arready : s1_arready);
+assign s0_arid    = ({4{(cur_slave_r == 0)}} & m_arid) & {4{(state_r == S_BUSY)}};
+assign s1_arid    = ({4{(cur_slave_r == 1)}} & m_arid) & {4{(state_r == S_BUSY)}};
+assign s0_araddr  = ({32{(cur_slave_r == 0)}} & m_araddr) & {32{(state_r == S_BUSY)}};
+assign s1_araddr  = ({32{(cur_slave_r == 1)}} & m_araddr) & {32{(state_r == S_BUSY)}};
+assign s0_arlen   = ({4{(cur_slave_r == 0)}} & m_arlen) & {4{(state_r == S_BUSY)}};
+assign s1_arlen   = ({4{(cur_slave_r == 1)}} & m_arlen) & {4{(state_r == S_BUSY)}};
+assign s0_arsize  = ({3{(cur_slave_r == 0)}} & m_arsize) & {3{(state_r == S_BUSY)}};
+assign s1_arsize  = ({3{(cur_slave_r == 1)}} & m_arsize) & {3{(state_r == S_BUSY)}};
+assign s0_arburst = ({2{(cur_slave_r == 0)}} & m_arburst) & {2{(state_r == S_BUSY)}};
+assign s1_arburst = ({2{(cur_slave_r == 1)}} & m_arburst) & {2{(state_r == S_BUSY)}};
+assign s0_arvalid = ((cur_slave_r == 0) & m_arvalid) & (state_r == S_BUSY);
+assign s1_arvalid = ((cur_slave_r == 1) & m_arvalid) & (state_r == S_BUSY);
 
-assign m_rid     =  {4{(state == S_BUSY)}} & ((cur_slave == 0) ? s0_rid : s1_rid);
-assign m_rdata   = {32{(state == S_BUSY)}} & ((cur_slave == 0) ? s0_rdata : s1_rdata);
-assign m_rresp   = {2{(state == S_BUSY)}}  & ((cur_slave == 0) ? s0_rresp : s1_rresp);
-assign m_rlast   =    (state == S_BUSY)    & ((cur_slave == 0) ? s0_rlast : s1_rlast);
-assign m_rvalid  =    (state == S_BUSY)    & ((cur_slave == 0) ? s0_rvalid : s1_rvalid);
+assign m_arready =  (state_r == S_BUSY) & ((cur_slave_r == 0) ? s0_arready : s1_arready);
 
-assign s0_rready = ((cur_slave == 0) & m_rready) & (state == S_BUSY);
-assign s1_rready = ((cur_slave == 1) & m_rready) & (state == S_BUSY);
+assign m_rid     = {4{(state_r == S_BUSY)}} & ((cur_slave_r == 0) ? s0_rid : s1_rid);
+assign m_rdata   = {32{(state_r == S_BUSY)}} & ((cur_slave_r == 0) ? s0_rdata : s1_rdata);
+assign m_rresp   = {2{(state_r == S_BUSY)}} & ((cur_slave_r == 0) ? s0_rresp : s1_rresp);
+assign m_rlast   = (state_r == S_BUSY) & ((cur_slave_r == 0) ? s0_rlast : s1_rlast);
+assign m_rvalid  = (state_r == S_BUSY) & ((cur_slave_r == 0) ? s0_rvalid : s1_rvalid);
 
-assign s0_awid    = ({4{(cur_slave == 0)}} & m_awid) & {4{(state == S_BUSY)}};
-assign s1_awid    = ({4{(cur_slave == 1)}} & m_awid) & {4{(state == S_BUSY)}};
-assign s0_awaddr  = ({32{(cur_slave == 0)}} & m_awaddr) & {32{(state == S_BUSY)}};
-assign s1_awaddr  = ({32{(cur_slave == 1)}} & m_awaddr) & {32{(state == S_BUSY)}};
-assign s0_awlen   = ({4{(cur_slave == 0)}} & m_awlen) & {4{(state == S_BUSY)}};
-assign s1_awlen   = ({4{(cur_slave == 1)}} & m_awlen) & {4{(state == S_BUSY)}};
-assign s0_awsize  = ({3{(cur_slave == 0)}} & m_awsize) & {3{(state == S_BUSY)}};
-assign s1_awsize  = ({3{(cur_slave == 1)}} & m_awsize) & {3{(state == S_BUSY)}};
-assign s0_awburst = ({2{(cur_slave == 0)}} & m_awburst) & {2{(state == S_BUSY)}};
-assign s1_awburst = ({2{(cur_slave == 1)}} & m_awburst) & {2{(state == S_BUSY)}};
-assign s0_awvalid = ((cur_slave == 0) & m_awvalid) & (state == S_BUSY);
-assign s1_awvalid = ((cur_slave == 1) & m_awvalid) & (state == S_BUSY);
+assign s0_rready = ((cur_slave_r == 0) & m_rready) & (state_r == S_BUSY);
+assign s1_rready = ((cur_slave_r == 1) & m_rready) & (state_r == S_BUSY);
 
-assign m_awready =    (state == S_BUSY)   &  ((cur_slave == 0) ? s0_awready : s1_awready);
+assign s0_awid    = ({4{(cur_slave_w == 0)}} & m_awid) & {4{(state_w == S_BUSY)}};
+assign s1_awid    = ({4{(cur_slave_w == 1)}} & m_awid) & {4{(state_w == S_BUSY)}};
+assign s0_awaddr  = ({32{(cur_slave_w == 0)}} & m_awaddr) & {32{(state_w == S_BUSY)}};
+assign s1_awaddr  = ({32{(cur_slave_w == 1)}} & m_awaddr) & {32{(state_w == S_BUSY)}};
+assign s0_awlen   = ({4{(cur_slave_w == 0)}} & m_awlen) & {4{(state_w == S_BUSY)}};
+assign s1_awlen   = ({4{(cur_slave_w == 1)}} & m_awlen) & {4{(state_w == S_BUSY)}};
+assign s0_awsize  = ({3{(cur_slave_w == 0)}} & m_awsize) & {3{(state_w == S_BUSY)}};
+assign s1_awsize  = ({3{(cur_slave_w == 1)}} & m_awsize) & {3{(state_w == S_BUSY)}};
+assign s0_awburst = ({2{(cur_slave_w == 0)}} & m_awburst) & {2{(state_w == S_BUSY)}};
+assign s1_awburst = ({2{(cur_slave_w == 1)}} & m_awburst) & {2{(state_w == S_BUSY)}};
+assign s0_awvalid = ((cur_slave_w == 0) & m_awvalid) & (state_w == S_BUSY);
+assign s1_awvalid = ((cur_slave_w == 1) & m_awvalid) & (state_w == S_BUSY);
 
-assign m_wready  =    (state == S_BUSY)   &  ((cur_slave == 0) ? s0_wready : s1_wready);
-assign m_bid     = {4{(state == S_BUSY)}} &  ((cur_slave == 0) ? s0_bid : s1_bid);
-assign m_bresp   = {2{(state == S_BUSY)}} &  ((cur_slave == 0) ? s0_bresp : s1_bresp);
-assign m_bvalid  =    (state == S_BUSY)   &  ((cur_slave == 0) ? s0_bvalid : s1_bvalid);
+assign m_awready = (state_w == S_BUSY) & ((cur_slave_w == 0) ? s0_awready : s1_awready);
 
-assign s0_wdata  = ({32{(cur_slave == 0)}} & m_wdata) & {32{(state == S_BUSY)}};
-assign s0_wstrb  = ({4{(cur_slave == 0)}} & m_wstrb) & {4{(state == S_BUSY)}};
-assign s0_wlast  = ((cur_slave == 0) & m_wlast) & (state == S_BUSY);
-assign s0_wvalid = ((cur_slave == 0) & m_wvalid) & (state == S_BUSY);
-assign s0_bready = ((cur_slave == 0) & m_bready) & (state == S_BUSY);
-assign s1_wdata  = ({32{(cur_slave == 1)}} & m_wdata) & {32{(state == S_BUSY)}};
-assign s1_wstrb  = ({4{(cur_slave == 1)}} & m_wstrb) & {4{(state == S_BUSY)}};
-assign s1_wlast  = ((cur_slave == 1) & m_wlast) & (state == S_BUSY);
-assign s1_wvalid = ((cur_slave == 1) & m_wvalid) & (state == S_BUSY);
-assign s1_bready = ((cur_slave == 1) & m_bready) & (state == S_BUSY);
+assign m_wready  = (state_w == S_BUSY) & ((cur_slave_w == 0) ? s0_wready : s1_wready);
+assign m_bid     = {4{(state_w == S_BUSY)}} & ((cur_slave_w == 0) ? s0_bid : s1_bid);
+assign m_bresp   = {2{(state_w == S_BUSY)}} & ((cur_slave_w == 0) ? s0_bresp : s1_bresp);
+assign m_bvalid  = (state_w == S_BUSY) & ((cur_slave_w == 0) ? s0_bvalid : s1_bvalid);
+
+assign s0_wdata  = ({32{(cur_slave_w == 0)}} & m_wdata) & {32{(state_w == S_BUSY)}};
+assign s0_wstrb  = ({4{(cur_slave_w == 0)}} & m_wstrb) & {4{(state_w == S_BUSY)}};
+assign s0_wlast  = ((cur_slave_w == 0) & m_wlast) & (state_w == S_BUSY);
+assign s0_wvalid = ((cur_slave_w == 0) & m_wvalid) & (state_w == S_BUSY);
+assign s0_bready = ((cur_slave_w == 0) & m_bready) & (state_w == S_BUSY);
+assign s1_wdata  = ({32{(cur_slave_w == 1)}} & m_wdata) & {32{(state_w == S_BUSY)}};
+assign s1_wstrb  = ({4{(cur_slave_w == 1)}} & m_wstrb) & {4{(state_w == S_BUSY)}};
+assign s1_wlast  = ((cur_slave_w == 1) & m_wlast) & (state_w == S_BUSY);
+assign s1_wvalid = ((cur_slave_w == 1) & m_wvalid) & (state_w == S_BUSY);
+assign s1_bready = ((cur_slave_w == 1) & m_bready) & (state_w == S_BUSY);
 
 endmodule
