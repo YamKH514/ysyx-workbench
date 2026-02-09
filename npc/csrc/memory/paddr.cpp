df5d27790bf24f320aa9a042d31e79945e9d249d
tracer-ysyx <tracer@ysyx.org>
2026-02-09 14:16:17 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-12-generic #12-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:46:52 UTC 2026 x86_64 GNU/Linux  14:16:17 up 4 days,  4:04,  1 user,  load average: 3.65, 3.07, 2.36

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -169,7 +169,6 @@ uint32_t paddr_read(uint32_t raddr)
 
 extern int npcmem_read(int raddr)
 {
-    printf("npcmem_read raddr=0x%08x\n", raddr);
     uint32_t addr = (uint32_t)raddr & ~0x3u;
     
     if(likely(in_pmem(addr)))
@@ -182,7 +181,6 @@ extern int npcmem_read(int raddr)
 
 extern void npcmem_write(int waddr, int wdata, char wmask)
 {
-    printf("npcmem_write waddr=0x%08x, wdata=0x%08x, wmask=0x%02x\n", waddr, wdata, wmask);
     uint32_t addr = (uint32_t)waddr & ~0x3u, data = (uint32_t)wdata;
     uint8_t  mask = (uint8_t) wmask;
     if(addr == SERIAL_PORT)
