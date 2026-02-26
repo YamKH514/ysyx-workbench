facf79681af7cf2409b74d1ca0905abd8194f582
tracer-ysyx <tracer@ysyx.org>
2026-02-26 14:51:33 +0800

    > compile NEMU ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 17:01:16 UTC 2026 x86_64 GNU/Linux  14:51:33 up 1 day, 21:24,  1 user,  load average: 1.40, 1.27, 1.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -174,6 +174,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 }
 
 extern void mem_dump() {
+  printf("mem dump start\n");
   FILE *fp;
   fp = fopen("mem_dump.bin", "wb");
   assert(fp != NULL);
