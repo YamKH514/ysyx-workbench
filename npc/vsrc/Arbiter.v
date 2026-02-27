242bb6e831105732826ea9d8f98fba67a3f4a166
tracer-ysyx <tracer@ysyx.org>
2026-02-27 19:43:04 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 17:01:16 UTC 2026 x86_64 GNU/Linux  19:43:04 up 23:17,  1 user,  load average: 2.06, 0.96, 0.69

--- npc/vsrc/Arbiter.v
+++ npc/vsrc/Arbiter.v
@@ -11,57 +11,46 @@ module Arbiter(
     output  reg         bg2_o
 );
 
-localparam S_IDLE = 1'd0;
-localparam S_BUSY = 1'd1;
+reg last_grant; // 0 -> device1, 1 -> device2
 
-reg state, next_state;
+assign bg1_o = state == S_BUSY & !last_grant;
+assign bg2_o = state == S_BUSY & last_grant;
 
 always @(posedge clk) begin
-    if (rst) state <= S_IDLE;
-    else state <= next_state;
-
     if (rst) begin
-        bg1_o <= 1'b0;
-        bg2_o <= 1'b0;
-    end else begin
-        case (state)
-            S_IDLE: begin
-                bg1_o <= 1'b0;
-                bg2_o <= 1'b0;
-                if (br1_i) begin
-                    bg1_o <= 1'b1;
-                end else if (br2_i) begin
-                    bg2_o <= 1'b1;
-                end
-            end
-            S_BUSY: begin
-                if (bs_i) begin
-                    bg1_o <= 1'b0;
-                    bg2_o <= 1'b0;
-                end
-            end
-            default: begin
-                bg1_o <= 1'b0;
-                bg2_o <= 1'b0;
-            end
+        last_grant <= 0;
+    end else if (state == S_IDLE) begin
+        case ({br2_i, br1_i})
+            2'b01: last_grant <= 1'b0;
+            2'b10: last_grant <= 1'b1;
+            2'b11: last_grant <= ~last_grant;
+            default: last_grant <= last_grant;
         endcase
     end
 end
 
-always @(*) begin
-    next_state = state;
-    case (state)
-        S_IDLE: begin
-            if (bs_i) begin
-                next_state = S_BUSY;
+localparam S_IDLE = 1'd0;
+localparam S_BUSY = 1'd1;
+
+reg state;
+
+always @(posedge clk) begin
+    if (rst) begin
+        state <= S_IDLE;
+    end else begin
+        case (state)
+            S_IDLE: begin
+                if (br1_i | br2_i) begin
+                    state <= S_BUSY;
+                end
             end
-        end
-        S_BUSY: begin
-            if (!bs_i) begin
-                next_state = S_IDLE;
+            S_BUSY: begin
+                if (bs_i) begin
+                    state <= S_IDLE;
+                end
             end
-        end
-    endcase
+        endcase
+    end
 end
 
 endmodule
