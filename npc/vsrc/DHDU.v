bee240ff8186c8e4e057f780d03bfe6c5c43700e
tracer-ysyx <tracer@ysyx.org>
2026-02-28 17:21:05 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 17:01:16 UTC 2026 x86_64 GNU/Linux  17:21:05 up 1 day, 20:55,  1 user,  load average: 2.70, 1.10, 0.65

---
+++ npc/vsrc/DHDU.v
@@ -0,0 +1,10 @@
+// Data Hazard Detection Unit
+module DHDU(
+    input clk,
+    input rst,
+
+    input [2:0] inst_type_i,
+    input [4:0] idu_r_reg1,
+    input [4:0] idu_r_reg2
+);
+endmodule
--- npc/vsrc/IF_ID.v
+++ npc/vsrc/IF_ID.v
@@ -49,6 +49,12 @@ always @(posedge clk) begin
         gpr_rdata1_r <= gpr_rdata1_i;
         gpr_rdata2_r <= gpr_rdata2_i;
         csr_rdata_r <= csr_rdata_i;
+    end else if (need_flush_i | (ifid_id_valid_o & ifid_id_ready_i)) begin
+        pc_r <= 'b0;
+        inst_r <= 'b0;
+        gpr_rdata1_r <= 'b0;
+        gpr_rdata2_r <= 'b0;
+        csr_rdata_r <= 'b0;
     end
 end
 
