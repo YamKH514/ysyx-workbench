004f1ad18ea1ab6ecf96efe01e0e132161407f86
tracer-ysyx <tracer@ysyx.org>
2026-03-02 17:24:46 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 17:01:16 UTC 2026 x86_64 GNU/Linux  17:24:46 up 3 days, 20:59,  1 user,  load average: 2.90, 2.50, 1.91

--- npc/vsrc/WBU.v
+++ npc/vsrc/WBU.v
@@ -33,7 +33,7 @@ module WBU(
 );
 
 assign wbu_csr_func3_o = lsu_wbu_csr_func3_i;
-assign wbu_csr_we_o = lsu_wbu_csr_we_i & (state == S_IDLE & lsu_wbu_valid_i);
+assign wbu_csr_we_o = lsu_wbu_csr_we_i & state == S_BUSY;
 assign wbu_csr_waddr_o = lsu_wbu_csr_waddr_i;
 assign wbu_csr_wdata_o = lsu_wbu_csr_wdata_i;
 assign wbu_csr_mepc_o = lsu_pc_i;
@@ -49,8 +49,8 @@ assign {ecall, mret, wbu_we, wbu_w_addr, wbu_wd_sel} = lsu_wbu_data_i;
 assign wbu_gpr_we_o = wbu_we & state == S_BUSY;
 assign wbu_gpr_waddr_o = wbu_w_addr;
 assign wbu_gpr_wdata_o = gpr_w_data;
-assign wbu_csr_ecall_o = ecall;
-assign wbu_csr_mret_o = mret;
+assign wbu_csr_ecall_o = ecall & state == S_BUSY;
+assign wbu_csr_mret_o = mret & state == S_BUSY;
 
 assign lsu_wbu_ready_o = state == S_BUSY;
 
