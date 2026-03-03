20d5434a3d4eb309f4e6011e04b31ea7698347ab
tracer-ysyx <tracer@ysyx.org>
2026-03-03 15:36:07 +0800

    > sim RTL ysyx_25120296 任堃厚 Linux ren-desktop 6.17.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 17:01:16 UTC 2026 x86_64 GNU/Linux  15:36:07 up 4 days, 19:10,  1 user,  load average: 0.88, 1.27, 1.18

--- npc/vsrc/ysyx_25120296.v
+++ npc/vsrc/ysyx_25120296.v
@@ -468,7 +468,7 @@ DHDU u_DHDU(
     .exls_mret          (exls_lsu_wbu_data[8]),
     .exls_is_load       (exls_lsu_data[5]),
     .lswb_rd_i         	(lswb_wbu_data[4:0]),
-    .lswb_we_i         	(lswb_wbu_data[7]),
+    .lswb_we_i         	(lswb_wbu_data[5]),
     .lswb_csr_waddr_i   (lswb_wbu_csr_waddr),
     .lswb_csr_we_i      (lswb_wbu_csr_we),
     .lswb_ecall         (lswb_wbu_data[7]),
